b4dec9011449b97a7a3f37f72437bec1a2cadff4
pollini <pollini@1eed4993-9cd0-4a5c-9189-545be6e0ecb2>
2014-12-16 10:53:37 +0000

    Fixed some errors in non linear solver
    git-svn-id: svn://www2.mate.polimi.it/bim++@27 1eed4993-9cd0-4a5c-9189-545be6e0ecb2

--- src/nonlinear_examples.cpp
+++ src/nonlinear_examples.cpp
@@ -5,20 +5,20 @@ void plaplacian::operator()(sparse_matrix& lhs, std::vector<double>& rhs,const s
 	
 	lhs.reset();
 	rhs.clear();
-	bim3a_structure (this->msh, lhs);
+	bim3a_structure (msh, lhs);
 	
 	std::vector<double> g;
-	bim3a_pde_gradient(this->msh,u,g);
+	bim3a_pde_gradient(msh,u,g);
 	
-	std::vector<double> modg(this->msh.nelements);
+	std::vector<double> modg(msh.nelements);
 	for(int i=0;i<this->msh.nelements;++i)
 		modg[i]=sqrt(g[3*i]*g[3*i]+g[3*i+1]*g[3*i+1]+g[3*i+2]*g[3*i+2]);
 	
-  std::vector<double> ecoeff (this->msh.nelements, 1.0); 
-	std::vector<double> isocoeff(this->msh.nelements,1.0); //isotropic diffusion coefficients
-	std::vector<double> dcoeff (this->msh.nelements*9,1.0);//anisotropic diffusion coefficients
+  std::vector<double> ecoeff (msh.nelements, 1.0); 
+	std::vector<double> isocoeff(msh.nelements,1.0); //isotropic diffusion coefficients
+	std::vector<double> dcoeff (msh.nelements*9,1.0);//anisotropic diffusion coefficients
 	
-	for (int i=0; i < this->msh.nelements;++i)
+	for (int i=0; i < msh.nelements;++i)
 		{
 
 			dcoeff[0+9*i]=pow(modg[i],p-4.0)*(p-2.0)*g[3*i+0]*g[3*i+0];
@@ -33,13 +33,12 @@ void plaplacian::operator()(sparse_matrix& lhs, std::vector<double>& rhs,const s
 			
 			isocoeff[i]=pow(modg[i],p-2.0);
 		}
-	bim3a_laplacian(this->msh,isocoeff,lhs);
+	bim3a_laplacian(msh,isocoeff,lhs);
 	bim3a_matrix_vector_product(lhs,u,rhs);
-	bim3a_dirichletBC(lhs,rhs,bnodes,bsol);
 	for(int i=0;i<rhs.size();++i)
 		rhs[i]*=-1;
-	bim3a_rhs(this->msh,ecoeff,f,rhs);
-	bim3a_laplacian_anisotropic (this->msh, dcoeff, lhs);
+	bim3a_rhs(msh,ecoeff,f,rhs);
+	bim3a_laplacian_anisotropic (msh, dcoeff, lhs);
 	bim3a_dirichletBC(lhs,rhs,bnodes,bsol);
 }
 
@@ -53,22 +52,22 @@ void plaplacian::operator()(std::vector<double>& F,const std::vector<double>& u)
 
 void plaplacian::get_solution(std::vector<double>& sol)
 {
-	sol=this->exactsol;
+	sol=exactsol;
 }
 
-void equation::operator()(sparse_matrix& lhs, std::vector<double>& rhs, std::vector<double>& u)
+void equation::operator()(sparse_matrix& lhs, std::vector<double>& rhs, const std::vector<double>& u)
 {
 	lhs.resize(1);
 	lhs[0][0]=2;
 	rhs.resize(1);
 	rhs[0]=-(u[0]*u[0]-2.0);
 }
-void equation::operator()(std::vector<double>& f, std::vector<double>& u)
+void equation::operator()(std::vector<double>& f, const std::vector<double>& u)
 {
 	f.resize(1);
 	f[0]=u[0]*u[0]-2.0;
 }
 void equation::get_solution(std::vector<double>& sol)
 {
-	sol=this->exactsol;
+	sol=exactsol;
 }
