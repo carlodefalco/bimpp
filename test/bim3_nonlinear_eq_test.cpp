67c5c7f3bc76c252431bff8e4cc3cad1b44c4bf7
pollini <pollini@1eed4993-9cd0-4a5c-9189-545be6e0ecb2>
2015-01-04 21:41:41 +0000

    code formatting
    git-svn-id: svn://www2.mate.polimi.it/bim++@39 1eed4993-9cd0-4a5c-9189-545be6e0ecb2

--- test/bim3_nonlinear_eq_test.cpp
+++ test/bim3_nonlinear_eq_test.cpp
@@ -1,12 +1,12 @@
 /*
   Copyright (C) 2011 Carlo de Falco
-  This software is distributed under the terms 
+  This software is distributed under the terms
   the terms of the GNU/GPL licence v3
 */
 /*
-  Problem:	x^2-2=0
-  
-  Exact Solution:	x=sqrt(2)
+  Problem:         x^2-2=0
+
+  Exact Solution:  x=sqrt(2)
 */
 
 #include <stdio.h>
@@ -25,24 +25,23 @@
 
 LIS_INT main(LIS_INT argc, char* argv[])
 {
-  int rank;
-  int size;
+  int rank, size;
 
   lis_initialize (&argc, &argv);
 
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Comm_size(MPI_COMM_WORLD,&size);
-	
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+
   std::vector<double> sol, exactsolution, uold;
 
   equation eq;
 
   inexact_newton_option option;
-  option.maxIter=100;
-  option.minRes=10e-10;
-  option.tol=10e-10;
-  option.forcing=0.5;
-  option.type=Inf;
+  option.maxIter = 100;
+  option.minRes = 10e-10;
+  option.tol = 10e-10;
+  option.forcing = 0.5;
+  option.type = Inf;
 
   inexact_newton_status status;
   stream_option stream = {2, "Solution_Equation.txt"};
@@ -59,11 +58,14 @@ LIS_INT main(LIS_INT argc, char* argv[])
       uold[0] = 0;
       eq.import (exactsolution);
       if(stream.verbosity >= 1)
-	std::cout << "\nResult of Non Linear Test \nwill be written in " << stream.filename << std::endl <<std::endl;
-    }			
+        std::cout << "\nResult of Non Linear Test"
+                  << " \nwill be written in " << stream.filename
+                  << std::endl << std::endl;
+    }
 
-  status = inexact_newton<equation, ForcingCostant, LIS> (eq, uold, sol, forcing, lis_option, option, stream);
-  if(rank == 0)
+  status = inexact_newton<equation, ForcingCostant, LIS>
+                 (eq, uold, sol, forcing, lis_option, option, stream);
+  if (rank == 0)
     {
       std::cout << "Solution: " << sol[0] << std::endl;
       std::cout << "Error: " << fabs (sol[0] - exactsolution[0]) << std::endl;
