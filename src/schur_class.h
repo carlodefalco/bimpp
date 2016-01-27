16cafe2bb6bf6681cb75ba130eb92f9ddd3a94cd
carlo <carlo@1eed4993-9cd0-4a5c-9189-545be6e0ecb2>
2016-01-27 15:33:00 +0000

    resolved conflict
    git-svn-id: svn://www2.mate.polimi.it/bim++@132 1eed4993-9cd0-4a5c-9189-545be6e0ecb2

--- src/schur_class.h
+++ src/schur_class.h
@@ -1,10 +1,11 @@
 /*
-  Copyright (C) 2015 Carlo de Falco
+  Copyright (C) 2016 Carlo de Falco
   This software is distributed under the terms
   the terms of the GNU/GPL licence v3
 */
+
 /*! \file schur_class.h
-  \brief wrapper for mumps data.
+  \brief Schur complement based linear solver.
 */
 
 #ifndef HAVE_SCHUR_CLASS_H
@@ -12,27 +13,61 @@
 
 #include "bim_sparse.h"
 #include "linear_solver.h"
+#include <mpi.h>
+#include <cstdio>
+#include <fstream>
 
-/// Class that solves a linear system using an intermediate Schur-complement elimination step.
+/// Schur complement linear solver.
+///
+/// The system is decomposed as :
+///
+///  [ A   a ] [ X ] = [ C ]
+///  [ b   B ] [ x ] = [ c ]
+///
+/// and the solution is computed via
+/// the following algorithm :
+/// 1) g := B \ c
+/// 2) G := B \ b
+/// 3) A += - a * G
+/// 4) C += - a * g
+/// 5) X = A \ C
+/// 6) x = g - G * X
+///
+/// Different solvers can be used
+/// to invert B in steps 1) and 2)
+/// than to invert A in step 5)
 class schur: public linear_solver
 {
+  
 private :
 
-  linear_solver* master;
-  linear_solver* slave;
-  
-public :
+  std::vector<int> idx_A, idx_B;
+  sparse_matrix matrix;
+  p_sparse_matrix A, a, B, b;
+  std::vector<double> C, c;
+  linear_solver *solver_A, *solver_B;
 
-  /// Init the (serial) mumps solver instance.
+  typedef struct
+  {
+    std::vector<double> a;
+    std::vector<int> i;
+    std::vector<int> j;
+  } aij_struct;
+
+  aij_struct A_aij, B_aij;
+  
+  int rank, size;
+  static const int index_base = 0;
+
+public :
+  
+  /// Init the solver instance.
   void
   init ();
 
   /// Default constructor.
-  schur (bool verbose_ = false, int icntl23_ = 0, int working_host_ = 1) :
-    linear_solver ("MUMPS", "direct"),
-    verbose (verbose_),
-    icntl23 (icntl23_),
-    working_host (working_host_)
+  schur ( ) :
+    linear_solver ("schur", "direct")    
   { init (); };
 
   /// Set-up the matrix structure.
@@ -55,6 +90,9 @@ public :
   void
   set_rhs (std::vector<double> &rhs);
 
+  void
+  set_initial_guess (std::vector<double> &guess_);
+
   /// Perform the factorization.
   int
   factorize ();
@@ -67,11 +105,16 @@ public :
   void
   cleanup ();
 
-  /// MUMPS uses 1-based indexing
+  /// uses 0-based indexing
   inline int
   get_index_base ()
   { return index_base; }
 
+  
+  /// print block-decomposed matrix
+  void
+  print_blocks (std::string basename = "block");
+  
 };
-
+  
 #endif
