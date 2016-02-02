9b26b8df82040194ad887e96c5c6a8733584b13d
carlo <carlo@1eed4993-9cd0-4a5c-9189-545be6e0ecb2>
2016-02-02 09:27:45 +0000

    schur
    git-svn-id: svn://www2.mate.polimi.it/bim++@135 1eed4993-9cd0-4a5c-9189-545be6e0ecb2

--- src/schur_class.cpp
+++ src/schur_class.cpp
@@ -1,119 +1,119 @@
 /*
-  Copyright (C) 2015 Carlo de Falco
+  Copyright (C) 2016 Carlo de Falco
   This software is distributed under the terms
   the terms of the GNU/GPL licence v3
 */
+
 /*! \file schur_class.cpp
-  \brief wrapper for mumps data.
+  \brief Schur complement based linear solver.
 */
 
-#include "mumps_class.h"
+#include "schur_class.h"
 
 void
-mumps::init ()
+schur::init ()
 {
-  id.job =  JOB_INIT;
-  id.par =   working_host;           // host working
-  id.sym =   0;                      // non symmetric
-  id.comm_fortran = F77_COMM_WORLD;  // MPI_COMM_WORLD
-
-  dmumps_c (&id);
-
-  // streams
-  int mult = verbose ? 1 : -1;
-  id.icntl[0] = mult * 6; // Output stream for error messages
-  id.icntl[1] = mult * 6; // Output stream for diagnostic messages
-  id.icntl[2] = mult * 6; // Output stream for global information
-  id.icntl[3] = mult * 3; // Level of printing
-
-  // Matrix input format
-  id.icntl[4]  =   0;
-  id.icntl[17] =   0;
-
-  // ordering
-  id.icntl[6] =  7; // scotch (3), or pord (4), or metis (5), or AMD (0), AMF (2), QAMD (6), AUTO (7)
-
-  // space for fill-in ---
-  id.icntl[13] = 300;
-  id.icntl[22] = (icntl23 > 0) ? icntl23 : 0;
-
-  // iterative refinement
-  id.icntl[9] = 25;
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
 }
 
 
 void
-mumps::set_lhs_structure
+schur::set_lhs_structure
 (int n,
  std::vector<int> &ir,
  std::vector<int> &jc,
  matrix_format_t f)
 {
-  if (f == csr)
+
+  if (rank == 0)
     {
-      id.n  = n;
-      id.nz = jc.size ();
-      id.irn = new int[jc.size ()];
-      id.jcn = &*jc.begin ();
-      for (int i = 0; i < n; ++i)
+      matrix.resize (n);
+      if (f == csr)
+        for (unsigned int i = 0; i < n; ++i)
+          for (unsigned int j = ir[i]; j < ir[i+1]; ++j)
+            matrix[i][jc[j]] = 0.0;
+      else
         {
-          for (int j = ir[i]; j < ir[i+1]; ++j)
-            id.irn[j - index_base] = i + index_base;
+          assert (ir.size () == jc.size ());
+          for (unsigned int i = 0; i < ir.size (); ++i)
+            matrix[ir[i]][jc[i]] = 0.0;
         }
+      
+      matrix.extract_block_pointer (idx_A, idx_A, A);
+      matrix.extract_block_pointer (idx_A, idx_B, a);
+      matrix.extract_block_pointer (idx_B, idx_A, b);
+      matrix.extract_block_pointer (idx_B, idx_B, B);
+      
+      A.aij (A_aij.a, A_aij.i, A_aij.j,
+             solver_A->get_index_base ());
+      
+      B.aij (B_aij.a, B_aij.i, B_aij.j,
+             solver_B->get_index_base ());
+      
     }
-  else
+}
+
+int
+schur::analyze ()
+{
+  solver_A->analyze ();
+  solver_B->analyze ();
+  return 0;
+}
+
+void
+schur::set_lhs_data (std::vector<double> &xa)
+{
+  if (rank == 0)
     {
-      id.n  = n;
-      id.nz = ir.size ();
-      id.irn = &*ir.begin ();
-      id.jcn = &*jc.begin ();
+      // update aij format
+      auto kk = xa.begin ();
+      for (auto rr = matrix.begin (); rr != matrix.end (); ++rr)
+        for (auto cc = rr->begin (); cc != rr->end (); ++cc)
+          cc->second = *(kk++);
+      
+      // update diagonal blocks and solvers
+      A.aij_update (A_aij.a, A_aij.i, A_aij.j,
+                    solver_A->get_index_base ());
+      solver_A->set_lhs_data (A_aij.a);
+
+      B.aij_update (B_aij.a, B_aij.i, B_aij.j,
+                    solver_B->get_index_base ());
+      solver_B->set_lhs_data (B_aij.a);
     }
-}
-
-int
-mumps::analyze ()
-{
-  id.job = JOB_ANALYZE;
-  dmumps_c (&id);
-  return id.info[0];
+  return;
 }
 
 void
-mumps::set_lhs_data (std::vector<double> &xa)
+schur::set_rhs (std::vector<double> &rhs)
 {
-  // Define LHS entries
-  id.a   = &*xa.begin ();
-}
+  if (rank == 0)
+    {
+      C.resize (idx_A.size ());
+      auto idx_C = C.begin ();
+      for (int ii = 0; ii < idx_A.size (); ++ii)
+        *(idx_C++) = rhs[idx_A[ii]];
+        
+      c.resize (idx_B.size ());
+      auto idx_c = c.begin ();
+      for (int ii = 0; ii < idx_B.size (); ++ii)
+        *(idx_c++) = rhs[idx_B[ii]];     
+    }
 
-void
-mumps::set_rhs (std::vector<double> &rhs)
-{
-  // Define RHS
-  id.rhs  =  &*rhs.begin ();
-  id.nrhs =  1;
-  id.lrhs =  rhs.size ();
-}
-
-int
-mumps::factorize ()
-{
-  id.job = JOB_FACTORIZE;
-  dmumps_c (&id);
-  return id.info[0];
+  return;
 }
 
 int
-mumps::solve ()
+schur::factorize ()
 {
-  id.job = JOB_SOLVE;
-  dmumps_c (&id);
-  return id.info[0];
+  int res_A = solver_A->factorize ();
+  int res_B = solver_B->factorize ();
+  return (res_A == 0 && res_B == 0) ? 0 : -1;
 }
 
+int
+schur::solve () { return 0; }
+
 void
-mumps::cleanup ()
-{
-  // clean up
-  id.job =  JOB_END;
-  dmumps_c (&id);
-}
+schur::cleanup () { return; }
