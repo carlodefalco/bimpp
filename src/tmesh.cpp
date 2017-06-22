ce66ae87fbbd6ee9d91865da21471b21d093d58e
Carlo de Falco <carlo.defalco@polimi.it>
2017-06-22 15:36:21 +0200

    add missing file

---
+++ src/tmesh.cpp
@@ -0,0 +1,123 @@
+/*
+  Copyright (C) 2017 Carlo de Falco
+  This software is distributed under the terms
+  the terms of the GNU/GPL licence v3
+*/
+
+/*! \file tmesh.cpp
+  \brief Interface for p4est library
+*/
+
+#include <tmesh.h>
+#include <array>
+
+/* Read a 2d p4est connectivity from a compressed octave
+ * binary file. The file should contain a struct
+ * named "msh" with fields "p" and "t". The former
+ * should be the list of vertex coordinates while the
+ * latter the list of element vertices.
+ *
+ * The following ordering is assumed for the nodes
+ * of each quadrilateral element.
+ *
+ *   4                   3
+ *   +-------------------+
+ *   |                   |
+ *   |                   |
+ *   |                   |
+ *   |                   |
+ *   |                   |
+ *   |                   |
+ *   +-------------------+
+ *   1                   2
+ */
+
+static void
+octbingz2connectivity (const char *filename, p4est_connectivity_t **conn)
+{
+    
+  // load data from file
+  octave_value tmp;
+  octave_io_mode m = gz_read_mode;
+  assert (octave_io_open (filename, m, &m) == 0);
+  assert (octave_load ("msh", tmp) == 0);
+
+  Matrix p_matrix =
+    tmp.scalar_map_value ().contents ("p").matrix_value ();
+
+  Array<int> t_matrix =
+    tmp.scalar_map_value ().contents ("t").array_value ();
+
+  p4est_topidx_t num_vertices = p_matrix.cols (),
+    num_trees = t_matrix.cols ();
+
+  std::array<int, 4> v = {0, 0, 0, 0};
+  int face = 0;
+
+  *conn =
+    p4est_connectivity_new (num_vertices, num_trees, 0, 0);
+
+  for (int node = 1; node <= num_vertices; ++node)
+    {
+      (*conn)->vertices [3 * (node - 1) + 0] =
+        p_matrix.fortran_vec ()[2 * (node - 1) + 0];
+      
+      (*conn)->vertices [3 * (node - 1) + 1] =
+        p_matrix.fortran_vec ()[2 * (node - 1) + 1];
+      
+      (*conn)->vertices [3 * (node - 1) + 2] = 0;
+    }
+
+  for (int element_number = 1;
+       element_number <= num_trees;
+       ++element_number)
+    {
+
+      for (int n = 0; n < 4; ++n)
+        v[n] = t_matrix.fortran_vec ()[5 * (element_number - 1) + n];
+
+      (*conn)->tree_to_vertex[4 * (element_number - 1) + 0] = v[0] - 1;
+      (*conn)->tree_to_vertex[4 * (element_number - 1) + 1] = v[1] - 1;
+      (*conn)->tree_to_vertex[4 * (element_number - 1) + 2] = v[3] - 1;
+      (*conn)->tree_to_vertex[4 * (element_number - 1) + 3] = v[2] - 1;
+
+    }
+
+  for (p4est_topidx_t tree = 0; tree < (*conn)->num_trees; ++tree)
+    for (face = 0; face < 4; ++face)
+      {
+        (*conn)->tree_to_tree[4 * tree + face] = tree;
+        (*conn)->tree_to_face[4 * tree + face] = face;
+      }
+
+  assert (p4est_connectivity_is_valid ((*conn)));
+  p4est_connectivity_complete ((*conn));
+};
+
+
+void
+tmesh::read_connectivity (const char *filename,
+                          int source,
+                          MPI_Comm comm)
+{
+
+  int rank, size;
+  MPI_Comm_rank (comm, &rank);
+  MPI_Comm_size (comm, &size);
+
+  if (rank == source)
+    octbingz2connectivity (filename, &conn);
+  
+  conn = p4est_connectivity_bcast (conn, source, comm);
+  p4est = p4est_new (comm, conn, 0, NULL, NULL);
+  
+};
+
+
+void
+tmesh::refine (int recursive = 0, int partforcoarsen = 0)
+{
+  p4est_refine (p4est, recursive, refine_fn, NULL);
+  p4est_balance (p4est, P4EST_CONNECT_FACE, NULL);
+  p4est_partition (p4est, partforcoarsen, NULL);
+};
