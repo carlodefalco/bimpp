a72b63cae936890fc1925c3cf9d509574db8be23
elauksap <pasquale.africa@libero.it>
2017-07-12 15:16:31 +0200

    Implementation complete + add reaction operator

--- src/quad_operators.h
+++ src/quad_operators.h
@@ -9,13 +9,18 @@
 #include <tuple>
 #include <vector>
 
-using func = std::function<double (const double &, const double &)>; // f(x, y).
-using dirichlet_bcs = std::vector<std::tuple<const int &, const int &, const func &>>;
+using func = std::function<double (double, double)>; // f(x, y).
+using dirichlet_bcs = std::vector<std::tuple<int, int, func>>;
 
 void bim2a_advection_diffusion (tmesh & mesh,
                                 const std::vector<double> & alpha,
                                 const std::vector<double> & psi,
                                 sparse_matrix & A);
+
+void bim2a_reaction (tmesh & mesh,
+                     const std::vector<double> & delta,
+                     const std::vector<double> & zeta,
+                     sparse_matrix & A);
 
 void bim2a_rhs (tmesh & mesh,
                 const std::vector<double> & f,
