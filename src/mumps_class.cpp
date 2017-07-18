2fa140070e2c7c03918e1d918b3ed7776675cd7a
elauksap <pasquale.africa@libero.it>
2017-07-18 12:19:44 +0200

    Distributed solver

--- src/mumps_class.cpp
+++ src/mumps_class.cpp
@@ -1,11 +1,11 @@
 /*
-  Copyright (C) 2011 Carlo de Falco
-  This software is distributed under the terms
-  the terms of the GNU/GPL licence v3
-*/
+ *  Copyright (C) 2011 Carlo de Falco
+ *  This software is distributed under the terms
+ *  the terms of the GNU/GPL licence v3
+ */
 /*! \file mumps_class.cpp
-  \brief wrapper for mumps data.
-*/
+ *  \brief wrapper for mumps data.
+ */
 
 #include "mumps_class.h"
 
@@ -16,27 +16,27 @@ mumps::init ()
   id.par =   working_host;           // host working
   id.sym =   0;                      // non symmetric
   id.comm_fortran = F77_COMM_WORLD;  // MPI_COMM_WORLD
-
+  
   dmumps_c (&id);
-
+  
   // streams
   int mult = verbose ? 1 : -1;
   id.icntl[0] = mult * 6; // Output stream for error messages
   id.icntl[1] = mult * 6; // Output stream for diagnostic messages
   id.icntl[2] = mult * 6; // Output stream for global information
   id.icntl[3] = mult * 3; // Level of printing
-
+  
   // Matrix input format
-  id.icntl[4]  =   0;
-  id.icntl[17] =   0;
-
+  id.icntl[4]  =   0; // Assembled format
+  id.icntl[17] =   0; // Localized on rank 0 (can be overwritten later)
+  
   // ordering
   id.icntl[6] =  7; // scotch (3), or pord (4), or metis (5), or AMD (0), AMF (2), QAMD (6), AUTO (7)
-
+  
   // space for fill-in ---
   id.icntl[13] = 300;
   id.icntl[22] = (icntl23 > 0) ? icntl23 : 0;
-
+  
   // iterative refinement
   id.icntl[9] = 25;
 }
@@ -50,25 +50,57 @@ mumps::set_lhs_structure
  matrix_format_t f)
 {
   if (f == csr)
+  {
+    id.n  = n;
+    id.nz = jc.size ();
+    id.irn = new int[jc.size ()];
+    id.jcn = &*jc.begin ();
+    for (int i = 0; i < n; ++i)
     {
-      id.n  = n;
-      id.nz = jc.size ();
-      id.irn = new int[jc.size ()];
-      id.jcn = &*jc.begin ();
-      for (int i = 0; i < n; ++i)
-        {
-          for (int j = ir[i]; j < ir[i+1]; ++j)
-            id.irn[j - index_base] = i + index_base;
-        }
+      for (int j = ir[i]; j < ir[i+1]; ++j)
+        id.irn[j - index_base] = i + index_base;
     }
+  }
   else
-    {
-      id.n  = n;
-      id.nz = ir.size ();
-      id.irn = &*ir.begin ();
-      id.jcn = &*jc.begin ();
-    }
+  {
+    id.n  = n;
+    id.nz = ir.size ();
+    id.irn = &*ir.begin ();
+    id.jcn = &*jc.begin ();
+  }
 }
+
+void
+mumps::set_lhs_distributed ()
+{ id.icntl[17] = 3; }
+
+void
+mumps::set_distributed_lhs_structure
+(int n,
+ std::vector<int> &ir,
+ std::vector<int> &jc,
+ matrix_format_t f)
+{
+  if (f == csr)
+  {
+    id.n  = n;
+    id.nz_loc = jc.size ();
+    id.irn_loc = new int[jc.size ()];
+    id.jcn_loc = &*jc.begin ();
+    for (int i = 0; i < n; ++i)
+      for (int j = ir[i]; j < ir[i+1]; ++j)
+        id.irn_loc[j - index_base] = i + index_base;
+  }
+  else
+  {
+    id.n  = n;
+    id.nz_loc = jc.size ();
+    id.irn_loc = &*ir.begin ();
+    id.jcn_loc = &*jc.begin ();
+  }
+}
+
+
 
 int
 mumps::analyze ()
@@ -83,6 +115,13 @@ mumps::set_lhs_data (std::vector<double> &xa)
 {
   // Define LHS entries
   id.a   = &*xa.begin ();
+}
+
+void
+mumps::set_distributed_lhs_data (std::vector<double> &xa)
+{
+  // Define LHS entries
+  id.a_loc   = &*xa.begin ();
 }
 
 void
