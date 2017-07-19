bdfd283a27ddc2c8225c8cb0a0cfe273c6c188ad
elauksap <pasquale.africa@libero.it>
2017-07-19 09:15:56 +0000

    Ready for parallel solving

--- test/p4est_operator_test_2.cpp
+++ test/p4est_operator_test_2.cpp
@@ -81,7 +81,7 @@ main (int argc, char **argv)
   
   // Assemble advection-diffusion matrix.
   sparse_matrix A;
-  A.resize(tmsh.num_owned_nodes());
+  A.resize(tmsh.num_global_nodes());
   
   double lambda = 25;
   std::vector<double> alpha(tmsh.num_local_quadrants (), 1);
@@ -100,7 +100,7 @@ main (int argc, char **argv)
   bim2a_advection_diffusion (tmsh, alpha, psi, A);
   
   // Assemble right-hand side.
-  std::vector<double> rhs(tmsh.num_local_nodes (), 0);
+  std::vector<double> rhs(tmsh.num_global_nodes (), 0);
   
   std::vector<double> f(tmsh.num_local_quadrants (), 0);
   std::vector<double> g(tmsh.num_local_nodes (), 0);
@@ -120,51 +120,35 @@ main (int argc, char **argv)
   bim2a_dirichlet_bc (tmsh, bcs, A, rhs);
   
   // Solve problem.
-  std::vector<double> vals;
-  std::vector<int> irow, jcol;
-  
-  A.aij(vals, irow, jcol, 1);
-  
-  MPI_Barrier(MPI_COMM_WORLD);
-  
   std::cout << "Solving linear system." << std::endl;
   
   mumps mumps_solver;
   
+  std::vector<double> vals;
+  std::vector<int> irow, jcol;
+  
+  A.aij(vals, irow, jcol, mumps_solver.get_index_base ());
+  
+  mumps_solver.set_lhs_distributed ();
+  mumps_solver.set_distributed_lhs_structure (A.rows (), irow, jcol);
+  mumps_solver.set_distributed_lhs_data (vals);
+  
+  // Reduce rhs (so that rank 0 has the actual rhs).
+  std::vector<double> global_rhs(tmsh.num_global_nodes(), 0);
+  MPI_Allreduce(rhs.data(), global_rhs.data(), rhs.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  
   if (rank == 0)
-    mumps_solver.set_lhs_structure(A.rows(), irow, jcol);
+    mumps_solver.set_rhs (global_rhs);
   
-  mumps_solver.analyze();
-  
-  if (rank == 0)
-    mumps_solver.set_lhs_data(vals);
-  
-  mumps_solver.factorize();
-  
-  if (rank == 0)
-    mumps_solver.set_rhs(rhs);
-  
-  mumps_solver.solve();
-  
-  // Compute error.
-  double local_error = 0;
-  double error = std::numeric_limits<double>::lowest ();
-  
-  for (auto quadrant = tmsh.begin_quadrant_sweep ();
-       quadrant != tmsh.end_quadrant_sweep ();
-       ++quadrant)
-    {
-      for (int ii = 0; ii < 4; ++ii)
-        {
-          local_error = std::abs(u_ex(quadrant->p(0, ii), quadrant->p(1, ii)) - rhs[quadrant->t(ii)]);
-          error = error < local_error ? local_error : error;
-        }
-    }
-  
-  std::cout << "L^inf-norm error: " << error << "." << std::endl;
+  // Solve.
+  mumps_solver.analyze ();
+  mumps_solver.factorize ();
+  mumps_solver.solve ();
+  mumps_solver.cleanup ();
   
   // Export solution.
-  tmsh.octbin_export ("p4est_operator_test_2_output", rhs);
+  MPI_Bcast(global_rhs.data(), global_rhs.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  tmsh.octbin_export ("p4est_operator_test_2_output", global_rhs);
   
   MPI_Finalize ();
   
