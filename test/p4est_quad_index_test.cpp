e64634aacd7d63ec4a4a0071c8a2af73e9f5631f
Carlo de Falco <carlo.defalco@polimi.it>
2017-07-19 10:31:30 +0200

    push forgotten test

---
+++ test/p4est_quad_index_test.cpp
@@ -0,0 +1,101 @@
+#include <bim_timing.h>
+#include <tmesh.h>
+
+#include <simple_connectivity_2d.h>
+
+#include <vector>
+#include <cassert>
+
+char filename[255];
+
+static int
+bottom_refinement (tmesh::quadrant_iterator quadrant)
+{
+  double ycoord;
+  double top = std::numeric_limits<double>::lowest ();
+  for (int ii = 0; ii < 4; ++ii)
+    {
+      ycoord = quadrant->p(1, ii);
+      top  = top < ycoord ? ycoord : top;
+    }
+  return ((top <= 0.5) ? 1 : 0);
+}
+
+static int
+left_refinement (tmesh::quadrant_iterator quadrant)
+{
+  double xcoord;
+  double right = std::numeric_limits<double>::lowest ();
+  for (int ii = 0; ii < 4; ++ii)
+    {
+      xcoord = quadrant->p(0, ii);
+      right  = right < xcoord ? xcoord : right;
+    }
+  return ((right <= 0.5) ? 1 : 0);
+}
+
+static int
+uniform_refinement (tmesh::quadrant_iterator quadrant)
+{ return 1; }
+
+int
+main (int argc, char **argv)
+{
+
+  int                   recursive, partforcoarsen, balance;
+  MPI_Comm              mpicomm = MPI_COMM_WORLD;  
+  int                   rank, size;
+  tmesh                 tmsh;
+  double                xcoord = 0.0;
+  double                ycoord = 0.0;
+
+  MPI_Init (&argc, &argv);
+
+  mpicomm = MPI_COMM_WORLD;
+  MPI_Comm_rank (mpicomm, &rank);
+  MPI_Comm_size (mpicomm, &size);
+
+  if (rank == 0)
+    write_example_connectivity ("p4est_conn_test_hanging.octbin.gz");
+
+  tmsh.read_connectivity ("p4est_conn_test_hanging.octbin.gz");
+
+  tmsh.set_refine_marker (uniform_refinement);
+  recursive = 0; partforcoarsen = 1;
+  tmsh.refine (recursive, partforcoarsen);
+  tmsh.refine (recursive, partforcoarsen);
+  
+  tmsh.set_refine_marker (bottom_refinement);
+  recursive = 0; partforcoarsen = 1;
+  tmsh.refine (recursive, partforcoarsen);
+  tmsh.refine (recursive, partforcoarsen);
+  
+  tmsh.set_refine_marker (left_refinement);
+  recursive = 0; partforcoarsen = 1;
+  tmsh.refine (recursive, partforcoarsen);
+  tmsh.refine (recursive, partforcoarsen);
+  
+  tmsh.vtk_export ("p4est_quad_index_test");
+  
+  int iel = 0;
+
+  for (int irank = 0; irank < size; ++irank)
+    {
+      if (irank == rank)
+        {
+          std::cout << "rank " << rank << ": " << std::endl;
+          for (auto quadrant = tmsh.begin_quadrant_sweep ();
+               quadrant != tmsh.end_quadrant_sweep ();
+               ++quadrant)
+            {
+              std::cout << "\tquad "
+                        << quadrant->get_global_quad_idx (mpicomm)
+                        << std::endl;
+            }
+        }
+      MPI_Barrier (mpicomm);
+    }
+        
+  MPI_Finalize ();
+  return 0;
+}
