e86b3eb6dcdd4046b2df93f254fc97e065f36a58
Carlo de Falco <carlo.defalco@polimi.it>
2017-11-29 09:09:41 +0100

    zz error markers

--- src/quad_operators.h
+++ src/quad_operators.h
@@ -12,22 +12,39 @@
 using func = std::function<double (double, double)>; // f(x, y).
 using dirichlet_bcs = std::vector<std::tuple<int, int, func>>;
 
-void bim2a_advection_diffusion (tmesh & mesh,
-                                const std::vector<double> & alpha,
-                                const std::vector<double> & psi,
-                                sparse_matrix & A);
+void
+bim2a_advection_diffusion (tmesh & mesh,
+                           const std::vector<double> & alpha,
+                           const std::vector<double> & psi,
+                           sparse_matrix & A);
 
-void bim2a_reaction (tmesh & mesh,
-                     const std::vector<double> & delta,
-                     const std::vector<double> & zeta,
-                     sparse_matrix & A);
+void
+bim2a_reaction (tmesh & mesh,
+                const std::vector<double> & delta,
+                const std::vector<double> & zeta,
+                sparse_matrix & A);
 
-void bim2a_rhs (tmesh & mesh,
-                const std::vector<double> & f,
-                const std::vector<double> & g,
-                std::vector<double> & rhs);
+void
+bim2a_rhs (tmesh & mesh,
+           const std::vector<double> & f,
+           const std::vector<double> & g,
+           std::vector<double> & rhs);
 
-void bim2a_dirichlet_bc (tmesh & mesh, const dirichlet_bcs & bcs,
-                         sparse_matrix & A, std::vector<double> & rhs);
+void
+bim2a_dirichlet_bc (tmesh & mesh, const dirichlet_bcs & bcs,
+                    sparse_matrix & A, std::vector<double> & rhs);
+
+int 
+zz_marker_grad (tmesh::quadrant_iterator q,
+                const std::vector<double> &dudxstar,
+                const std::vector<double> &dudystar,
+                const std::vector<double> &u,
+                double limit);
+
+int 
+zz_marker_sol (tmesh::quadrant_iterator q,
+               const std::vector<double> &ustar,
+               const std::vector<double> &u,
+               double limit);
 
 #endif
