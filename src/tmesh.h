d271b4337def226d38bcb7c7651f5d263c0a0031
Carlo de Falco <carlo.defalco@polimi.it>
2017-12-01 13:37:44 +0100

    example prototype for distributed array

--- src/tmesh.h
+++ src/tmesh.h
@@ -399,24 +399,24 @@ distributed_vector
 public :
 
   p4est_locidx_t
-  num_local_entries
+  num_local_entries ()
   { return data_location::num_local_entries (the_tmesh); }
 
   p4est_gloidx_t
-  num_global_entries
+  num_global_entries ()
   { return data_location::num_global_entries (the_tmesh); }
 
   T&
   index_local (p4est_locidx_t);
 
   const T&
-  index_local (p4est_locidx_t);
+  index_local (p4est_locidx_t) const;
 
   T&
   index_global (p4est_gloidx_t);
 
   const T&
-  index_global (p4est_gloidx_t);
+  index_global (p4est_gloidx_t) const;
 
   void
   synchronize ();
@@ -425,6 +425,7 @@ public :
   all_reduce ();
 
 private :
+
   tmesh *the_tmesh;
   // should probably use some sort of smart pointer here
   T *data;
