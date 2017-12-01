7a7ddaa4620bb06b0f08a3c0c0349aa79520b67a
Carlo de Falco <carlo.defalco@polimi.it>
2017-12-01 13:33:25 +0100

    indentation and formatting

--- src/quad_operators.h
+++ src/quad_operators.h
@@ -9,50 +9,54 @@
 #include <tuple>
 #include <vector>
 
-using func = std::function<double (double, double)>; // f(x, y).
+/// f(x, y).
+using func = std::function<double (double, double)>; 
 using dirichlet_bcs = std::vector<std::tuple<int, int, func>>;
 using q1_vec = std::vector<double>;
 using gradient = std::pair<q1_vec, q1_vec>;
-using q2_vec = std::vector<std::array<double, 9>>; // Nodes, faces, cell midpoint dofs.
+/// Nodes, faces, cell midpoint dofs.
+using q2_vec = std::vector<std::array<double, 9>>;
 
 void
 bim2a_advection_diffusion (tmesh & mesh,
-                           const std::vector<double> & alpha,
-                           const std::vector<double> & psi,
-                           sparse_matrix & A);
+                           const std::vector<double>& alpha,
+                           const std::vector<double>& psi,
+                           sparse_matrix& A);
 
 void
-bim2a_reaction (tmesh & mesh,
-                const std::vector<double> & delta,
-                const std::vector<double> & zeta,
-                sparse_matrix & A);
+bim2a_reaction (tmesh& mesh,
+                const std::vector<double>& delta,
+                const std::vector<double>& zeta,
+                sparse_matrix& A);
 
 void
-bim2a_rhs (tmesh & mesh,
-           const std::vector<double> & f,
-           const std::vector<double> & g,
-           std::vector<double> & rhs);
+bim2a_rhs (tmesh& mesh,
+           const std::vector<double>& f,
+           const std::vector<double>& g,
+           std::vector<double>& rhs);
 
 void
-bim2a_dirichlet_bc (tmesh & mesh, const dirichlet_bcs & bcs,
-                    sparse_matrix & A, std::vector<double> & rhs);
+bim2a_dirichlet_bc (tmesh& mesh, const dirichlet_bcs& bcs,
+                    sparse_matrix& A, std::vector<double>& rhs);
 
-gradient bim2c_quadtree_pde_recovered_gradient(tmesh & mesh,
-                                               const q1_vec & u);
+gradient
+bim2c_quadtree_pde_recovered_gradient (tmesh& mesh,
+                                       const q1_vec& u);
 
-q2_vec   bim2c_quadtree_pde_recovered_solution(tmesh & mesh,
-                                               const gradient & du);
+q2_vec
+bim2c_quadtree_pde_recovered_solution (tmesh& mesh,
+                                       const gradient& du);
 
 int 
 zz_marker_grad (tmesh::quadrant_iterator q,
-                const gradient & du_star,
-                const q1_vec & u,
+                const gradient& du_star,
+                const q1_vec& u,
                 double limit);
 
 int 
 zz_marker_sol (tmesh::quadrant_iterator q,
-               const q2_vec & ustar,
-               const q1_vec & u,
+               const q2_vec& ustar,
+               const q1_vec& u,
                double limit);
 
 #endif
