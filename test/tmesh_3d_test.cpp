ccee62c63f44dcf288d3ea02b5542e3c4b349317
Cesare Giannetti <taneggi@gmail.com>
2018-02-23 10:22:27 +0000

    added compile test for tmesh_3d but got segmentation fault

---
+++ test/tmesh_3d_test.cpp
@@ -0,0 +1,14 @@
+#include <tmesh_3d.h>
+
+int
+main (int argc, char **argv)
+{
+  MPI_Init (&argc, &argv);
+  
+  int                   recursive, partforcoarsen, balance;
+  MPI_Comm              mpicomm = MPI_COMM_WORLD;  
+  int                   rank, size;
+  tmesh_3d              tmsh;
+  
+  return 0;
+}
