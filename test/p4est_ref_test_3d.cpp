ada642b71cd4f4e2d27264d8bbbadbe0354011b6
Cesare Giannetti <taneggi@gmail.com>
2018-03-15 19:59:13 +0000

    fixed function name

--- test/p4est_ref_test_3d.cpp
+++ test/p4est_ref_test_3d.cpp
@@ -57,7 +57,7 @@ main (int argc, char **argv)
   MPI_Comm_size (mpicomm, &size);
 
   if (rank == 0)
-    write_example_connectivity ("p4est_ref_test_3d.octbin.gz");
+    write_example_connectivity3 ("p4est_ref_test_3d.octbin.gz");
 
   tmsh.read_connectivity ("p4est_ref_test_3d.octbin.gz");
 
