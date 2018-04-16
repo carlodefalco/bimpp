9bbac12b5926498afe46abd90099564dcc8e2912
elauksap <pasquale.africa@libero.it>
2018-04-16 12:10:07 +0000

    Rename userint_replace + various fixes

--- examples/adaptive_adr_2d/p4est_dr_test_4_uniform.cpp
+++ examples/adaptive_adr_2d/p4est_dr_test_4_uniform.cpp
@@ -32,7 +32,7 @@ main (int argc, char **argv)
   tmsh.read_connectivity (simple_conn_p, simple_conn_num_vertices,
                           simple_conn_t, simple_conn_num_trees);
   
-  tmsh.set_replace_fun (tmesh::userint_replace);
+  tmsh.set_replace_fun (tmesh::user_int_replace);
   
   recursive = 0; partforcoarsen = 1;
   for (int cycle = 0; cycle < 2; ++cycle)
@@ -56,8 +56,8 @@ main (int argc, char **argv)
       std::cout << "*** Step " << adapt << " ***" << std::endl;
       
       // Compute coefficients.
-      double eps1 = 5e-7;
-      double eps2 = 1e-6;
+      double eps1 = 5e-4;
+      double eps2 = 1e-1;
       
       double c = -0.4375 * eps2 /
         (0.5 * std::sqrt(eps1) * std::cosh(0.5 / std::sqrt(eps1)) +
@@ -295,7 +295,7 @@ main (int argc, char **argv)
       
       std::cout << " Done." << std::endl;
       
-      if (tmsh.num_global_nodes () >= 1e6)
+      if (tmsh.num_global_nodes () >= 1e7)
         break;
       
       // Refine.
