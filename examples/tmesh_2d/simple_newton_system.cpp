ad371c2893b4b2ae846a6d90323834e9ce9bd41b
TemeAvramova <nushy_avramova@hotmail.it>
2018-04-26 11:53:35 +0200

    I added the system version of simple_newton

---
+++ examples/tmesh_2d/simple_newton_system.cpp
@@ -0,0 +1,398 @@
+/*
+  Copyright (C) 2018 Carlo de Falco
+  This software is distributed under the terms
+  the terms of the GNU/GPL licence v3
+*/
+
+#include <cmath>
+
+#include <bim_timing.h>
+#include <mumps_class.h>
+#include <tmesh.h>
+#include <quad_operators.h>
+
+
+constexpr int NUM_REFINEMENTS = 5;
+constexpr double MIN_RESIDUAL = 1.e-6;
+constexpr int NT              = 10;
+constexpr int MAX_IT          = 50;
+constexpr double DT           = 1.0e-2;
+
+constexpr double T0    = 0.;
+constexpr double T     = 10.;
+constexpr double eps_u = 1.0e-2;
+
+constexpr p4est_topidx_t simple_conn_num_vertices = 4;
+constexpr p4est_topidx_t simple_conn_num_trees = 1;
+const double simple_conn_p[simple_conn_num_vertices*2] =
+  {0., 0., 1., 0.,  1., 1., 0., 1.};
+const p4est_topidx_t simple_conn_t[simple_conn_num_trees*5] =
+  {1, 2, 3, 4, 1};
+
+static int
+uniform_refinement (tmesh::quadrant_iterator q)
+{ return NUM_REFINEMENTS; }
+
+bool
+all_non_negative (std::vector<double> vect)
+{
+  for (int i = 0; i < vect.size () ; ++i)
+    if (vect[i] < 0)
+      return 0;
+  return 1; 
+}
+
+
+int
+main (int argc, char **argv)
+{
+
+  MPI_Init (&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+  /// Generate the mesh in 2d
+  tmesh tmsh;
+  tmsh.read_connectivity (simple_conn_p, simple_conn_num_vertices,
+                          simple_conn_t, simple_conn_num_trees);
+
+  int recursive = 1;
+  tmsh.set_refine_marker (uniform_refinement);
+  tmsh.refine (recursive);
+
+  tmesh::idx_t n_nodes = tmsh.num_global_nodes ();
+  tmesh::idx_t n_elements = tmsh.num_local_quadrants ();
+
+  /// Time stepping variables
+  double t           = T0;
+  double told        = T0;
+  double tvold       = T0;
+  double dt          = DT;
+  int nt             = NT;
+
+  std::array<double, NT+1> t_save {t};
+  std::vector<double> t_vect;
+  double dt_tsave = ((T - T0) / NT);
+  
+  auto its = [&t, &dt_tsave] ()
+    {double out = t; t += dt_tsave ; return out; };
+  std::generate (t_save.begin (), t_save.end (), its);
+  t = T0;
+  
+  /// Newton variables
+  double              residual_norm;
+  int                 it_nonlin;
+  auto compute_norm = [&residual_norm] (double x)
+    { residual_norm += std::pow (x, 2); };
+
+  /// PDE arrays system
+  // (1/dt) * mass * (u - uold) + laplacian (eps_u) * u + mass * (exp (u - v)) = 1
+  // (1/dt) * mass * (v - vold) + laplacian (eps_v) * v - mass * (exp (u - v)) = 0
+  //
+  // Auu = laplacian (eps_u) + mass (1/dt + exp (u - v))
+  //      |_________________| |________________________|
+  //             A0                     A1
+  //
+  // Auv =  mass (- exp (u - v))
+  //       |________________________|
+  //                 A1
+  //
+  // Avu =  mass (- exp (u - v))
+  //      |________________________|
+  //                 A1
+  //
+  // Avv = laplacian (eps_v) + mass (1/dt - exp (u - v))
+  //      |_________________| |________________________|
+  //             A0                     A1
+  //
+  //
+  // fu = - mass ((1/dt) * (u - uold) + exp (u - v) - 1)  -  laplacian (eps_u) * u
+  //        |_________________________________________|      |___________________|
+  //                 f0                                            f1
+  //
+  // fv = - mass ((1/dt) * (v - vold) - exp (u - v))  -  laplacian (eps_v) * v
+  //        |______________________________________|    |_____________________|
+  //                 f0                                            f1
+  //
+  // A * [du dv] = [fu fv] ,  x = [u v]
+  // [u v] = [u v] + [du dv], dx = [du dv]
+
+  std::vector<double> xold, xvold, xguess;
+  xold.assign (2 * n_nodes, 1.0);
+  xvold.assign (2 * n_nodes, 1.0);
+  xguess.assign (2 * n_nodes, 1.0);
+  
+  std::vector<double> x (xold);
+  std::vector<double> dx (xold);
+  dx.assign (2 * n_nodes, 0.0);
+  
+  auto ixu  = x.begin ();
+  auto ixv  = x.begin () + n_nodes;
+  auto idx = x.begin ();
+  auto ixo = xold.begin ();
+
+  auto rhsfun_u = [&ixu, &ixv, &ixo, &dt] ()
+    {return -(*ixu - *(ixo++)) / dt - std::exp (*(ixu++) - *(ixv++)) + 1.0; };
+
+  auto rhsfun_v = [&ixu, &ixv, &ixo, &dt] ()
+    {return -(*ixu - *(ixo++)) / dt + std::exp (*(ixu++) - *(ixv++)); };
+
+
+  double dtinv = 1 / dt;
+  auto expudtinv_u = [&ixu, &ixv, &dtinv] ()
+    { return  std::exp (*(ixu++) - *(ixv++)) + dtinv; };
+
+  auto expudtinv_v = [&ixu, &ixv, &dtinv] ()
+    { return - std::exp (*(ixu++) - *(ixv++)) + dtinv; };
+
+  
+  /// Allocate system matrix
+  mumps *lin_solver = new mumps ();
+
+  int index_base = lin_solver->get_index_base ();
+  std::vector<double> xa, xaA;
+  std::vector<int> ir, jc, irA, jcA;
+  
+  sparse_matrix A_block, A;
+  A_block.resize (n_nodes);
+  A (2 * n_nodes);
+  dirichlet_bcs bcs;
+  for (int i = 0; i < 4; ++i)
+    bcs.push_back (std::make_tuple (0, i, [](double x, double y){return .0;}));
+  
+  bim2a_structure (tmsh, A_block);
+  A_block.aij (xa, ir, jc, index_base);
+  for (int ii = 0; ii < 2; ++ii)
+    for (int jj = 0; jj < 2; ++jj)
+      for (int i = 0; i < n_nodes; ++i)
+	  A[ir + ii * n_nodes - index_base][jc + jj * n_nodes - index_base] = 0;
+
+  
+  std::vector<double> ecoeff (n_nodes);
+  std::vector<double> ncoeff (n_elements);
+  std::vector<double> f (n_nodes);
+
+  ecoeff.assign (n_elements, 1.0);
+  ncoeff.assign (n_nodes, 1.0);
+  f.assign (n_nodes, 0.0);
+
+  A.aij (xaA, irA, jcA, index_base);
+  lin_solver->set_lhs_distributed ();
+  lin_solver->set_distributed_lhs_structure (A.rows (), irA, jcA);
+  lin_solver->analyze ();
+  
+  int isave = 0;
+  t_vect.push_back (t);
+  t += dt;
+  /*
+  for (auto t_save_p = t_save.begin (); t_save_p != t_save.end (); ++t_save_p)
+    {
+
+      while (told < (*t_save_p))
+        {
+          if (rank == 0)
+            {
+              if (t > (*t_save_p)) t = (*t_save_p);
+	      dt = t - told;
+	      
+            }
+	  if (rank == 0)
+	    std::cout << "TIME : "<< t << std::endl;
+	    
+          MPI_Bcast (&t, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	  MPI_Bcast (&dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	  MPI_Bcast (&dtinv, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	  MPI_Barrier (MPI_COMM_WORLD);
+
+	  if (told != tvold)
+	    for (int i = 0; i < n_nodes ; ++i)
+	      uguess[i] = (t - tvold ) /
+		(told - tvold) * uold[i] +
+		dt / (tvold - told) * uvold[i];                              
+	    
+	  if (!all_non_negative (uguess))
+	    {
+	      if (rank == 0)
+		std::cout << "Negative guess" <<std::endl;
+	      for (int i = 0; i < n_nodes; ++i)
+		uguess[i] = std::max (0.0, uguess[i]);
+	    }
+
+	  while (true) //////////////////////////////////////////////////////////////////
+	    {
+	      for (it_nonlin = 0; it_nonlin < MAX_IT; ++it_nonlin)
+		{
+		  
+		  if (rank == 0) tic ();
+		  f.assign (n_nodes, 0.0);
+		  A.reset ();
+
+		  // A0
+		  ecoeff.assign (n_elements, eps_u);
+		  ncoeff.assign (n_nodes, 1.0);
+		  bim2a_advection_diffusion (tmsh, ecoeff, ncoeff, A);
+              
+		  // f1
+		  sparse_matrix::col_iterator ja;
+		  for (unsigned int ia = 0; ia < A.size (); ++ia)
+		    if (A[ia].size ())
+		      for (ja = A[ia].begin (); ja != A[ia].end (); ++ja)
+			f[ia] -= A.col_val (ja) * u[A.col_idx (ja)];
+
+		  // f0
+		  ecoeff.assign (n_elements, 1.0);
+		  iu = u.begin ();
+		  iuo = uguess.begin ();
+		  
+		  std::generate (ncoeff.begin (), ncoeff.end (), rhsfun);
+		  bim2a_rhs (tmsh, ecoeff, ncoeff, f);
+
+		  // A1
+		  ecoeff.assign (n_elements, 1.0);
+		  iu = u.begin ();
+		  iuo = uguess.begin ();
+		  dtinv = 1 / dt;
+	          std::generate (ncoeff.begin (), ncoeff.end (), expudtinv);
+		  bim2a_reaction (tmsh, ecoeff, ncoeff, A);             
+		  MPI_Barrier (MPI_COMM_WORLD);
+		  if (rank == 0) toc ("assembly");
+              
+		  // TODO: MPI_Reduce f on rank 0
+
+		  if (rank == 0) tic ();
+		  std::copy (f.begin (), f.end (), du.begin ());
+		  lin_solver->set_rhs (du);
+
+		  bim2a_dirichlet_bc (tmsh, bcs, A, du);
+        
+
+		  A.aij_update (xa, ir, jc, lin_solver->get_index_base ());
+
+		  lin_solver->set_distributed_lhs_data (xa);
+
+		  lin_solver->factorize ();
+		  lin_solver->solve ();
+		  MPI_Barrier (MPI_COMM_WORLD);
+		  if (rank == 0) toc ("solve");
+
+		  if (rank == 0)
+		    {
+		      tic ();
+		      residual_norm = 0.0;
+		      std::for_each (du.begin (), du.end (), compute_norm);
+		      residual_norm = std::sqrt (residual_norm);
+		    }
+		  MPI_Bcast (&residual_norm, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+              
+		  for (int i = 0; i < n_nodes; ++i)
+		    u[i] += du[i];
+		  
+		  MPI_Barrier (MPI_COMM_WORLD);
+		  if (rank == 0) toc ("increment");
+              
+		  if (rank == 0)
+		    std::cout << "iteration = "
+			      << it_nonlin
+			      << " incr norm = "
+			      << residual_norm
+			      << std::endl;
+		  if (residual_norm <= MIN_RESIDUAL) break;
+                        
+		}
+	      if (residual_norm <= MIN_RESIDUAL && all_non_negative (u))
+	        break;
+	      else
+		{
+		  if (rank == 0)
+		    {
+		      dt *= 0.5;
+		      dtinv = 1 / dt;
+		      t = told + dt;
+		      std::cout << "Reducing time step : t = " << t << ", dt = "<< dt << std::endl;
+		    }
+		  MPI_Bcast (&dtinv, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		  MPI_Bcast (&dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		  MPI_Barrier (MPI_COMM_WORLD);
+
+		  if (told != tvold)
+		    {
+		      for (int i = 0; i < n_nodes ; ++i)
+			uguess[i] = (t - tvold ) /
+			  (told - tvold) * uold[i] +
+			  dt / (tvold - told) * uvold[i];                              
+	    
+		      if (!all_non_negative (uguess))
+			{
+			  if (rank == 0)
+			    std::cout << "Negative guess" <<std::endl;
+			  for (int i = 0; i < n_nodes; ++i)
+			    uguess[i] = std::max (0.0, uguess[i]);
+			    
+			}
+		    }
+		  std::copy (uold.begin (), uold.end (), u.begin ());
+         
+		}
+	    }
+	  
+          std::copy (uold.begin (), uold.end (), uvold.begin ());
+          std::copy (u.begin (), u.end (), uold.begin ());
+     	  tvold = told;
+          told = t;
+	  t_vect.push_back (t);
+	  if (rank == 0)
+	    {
+	      if (residual_norm < 1e-14)
+		dt *= 2;
+	    
+	      else
+		dt = dt * std::min (std::sqrt(.38) * std::sqrt (MIN_RESIDUAL / residual_norm), 2.0);
+	      
+	      dt = std::min (dt , dt_tsave);
+	      t += dt;
+	      std::cout <<"----dt = "<< dt << std::endl;    
+	      std::cout <<"----final step error = "<< residual_norm << std::endl;    
+	    }
+	  MPI_Bcast (&t, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	  MPI_Bcast (&dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	  MPI_Barrier (MPI_COMM_WORLD);
+
+	}
+
+      
+      // std::cout << "SALVO AL TEMPO "<< t <<std::endl;
+
+      //      if (rank == 0) tic ();
+
+        tmsh.octbin_export ((std::string ("tumor_growth_u_")
+                             + std::to_string (isave)).c_str (), u);
+
+        tmsh.octbin_export ((std::string ("tumor_growth_f_")
+                             + std::to_string (isave)).c_str (), f);
+        
+        tmsh.octbin_export ((std::string ("tumor_growth_du_")
+                             + std::to_string (isave++)).c_str (), du);
+
+      MPI_Barrier (MPI_COMM_WORLD);  
+      //      if (rank == 0) toc ("export");
+
+    }
+  std::cout <<"--- t_vect --- " << std::endl; 
+  for (int i = 0 ; i < t_vect.size () ; ++i)
+    std::cout<<t_vect[i]<<std::endl;
+
+  std::cout <<"--- t_save --- " << std::endl;
+  for (int i = 0 ; i < t_save.size () ; ++i)
+    std::cout<<t_save[i]<<std::endl;
+  */
+  print_timing_report ();
+  lin_solver->cleanup ();
+  MPI_Finalize ();
+
+  return 0;
+}
+
+
+
