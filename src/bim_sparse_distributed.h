fb41d5346c283cf56cfdb08745140fccf4a9317b
Carlo de Falco <carlo.defalco@polimi.it>
2018-05-15 17:48:22 +0200

    distributed sparse matrix

---
+++ src/bim_sparse_distributed.h
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2018 Carlo de Falco
+  This software is distributed under the terms
+  the terms of the GNU/GPL licence v3
+*/
+
+#ifndef HAVE_BIM_SPARSE_DISTRIBUTED_H
+#define HAVE_BIM_SPARSE_DISTRIBUTED_H 1
+
+#include <mpi.h>
+#include <bim_sparse.h>
+
+class
+distributed_sparse_matrix
+  : public sparse_matrix
+{
+
+private :
+
+  void
+  non_local_csr ( );
+  
+
+  size_t is, ie;
+  MPI_Comm comm;
+  int mpirank, mpisize;
+    
+public :
+
+  struct
+  non_local_t
+  {
+    std::vector<int> row_ptr, col_ind;
+    std::vector<double> a;
+  } non_local;
+
+  std::map<int, std::vector<int>> row_buffers;
+  std::map<int, std::vector<int>> col_buffers;
+  std::map<int, std::vector<double>> val_buffers;
+
+  std::vector<int> ranges;
+  std::vector<int> rank_nnz;
+
+  void
+  set_ranges (size_t is_, size_t ie_, MPI_Comm comm_)
+  {
+    is = is_; ie = ie_; comm = comm_;
+    MPI_Comm_rank (comm, &mpirank);
+    MPI_Comm_size (comm, &mpisize);
+  }
+
+  void
+  assemble ();
+
+};
+
+#endif
