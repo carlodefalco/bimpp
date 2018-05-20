a07df4ff9e1bc22963832964cc9762dcc05a98f5
TemeAvramova <nushy_avramova@hotmail.it>
2018-05-20 10:43:05 +0200

    Add deallocation of non_local.a and non_local.col_ind, add non_local_csr_update and update_assemble

--- src/bim_sparse_distributed.cpp
+++ src/bim_sparse_distributed.cpp
@@ -16,6 +16,7 @@ distributed_sparse_matrix::set_ranges (size_t is_,
   MPI_Comm_rank (comm, &mpirank);
   MPI_Comm_size (comm, &mpisize);
 
+
   /// Gather ranges
   ranges.assign (mpisize + 1, 0);
   MPI_Allgather (&ie, 1, MPI_INT, &(ranges[1]), 1, MPI_INT, comm);
@@ -26,6 +27,13 @@ distributed_sparse_matrix::set_ranges (size_t is_,
 void
 distributed_sparse_matrix::non_local_csr ()
 {
+
+  std::vector<int> temp1;
+  std::vector<double> temp2;
+
+  non_local.col_ind.swap (temp1);
+  non_local.a.swap (temp2);
+  
   this->set_properties ();
   non_local.a.reserve (this->nnz / this->mpisize);
   non_local.col_ind.reserve (this->nnz / this->mpisize);
@@ -34,26 +42,40 @@ distributed_sparse_matrix::non_local_csr ()
   int idx = 0, idr = 0;
 
   sparse_matrix::col_iterator jj;
-
+   
   for (size_t ii = 0; ii < this->size (); ++ii)
     {
       non_local.row_ptr[idr++] = idx;
+ 
       if (((*this)[ii].size () > 0) && ((ii < is) || (ii >= ie)))
-        {
-          for (jj  = (*this)[ii].begin ();
-               jj != (*this)[ii].end (); ++jj)
-            {
-              non_local.col_ind.push_back (this->col_idx (jj));
-              non_local.a.push_back (this->col_val (jj));
-              idx++;
-            }
-        }
+	{
+	  for (jj  = (*this)[ii].begin ();
+	       jj != (*this)[ii].end () ; ++jj)
+	    {
+	      non_local.col_ind.push_back (this->col_idx (jj));
+	      non_local.a.push_back (this->col_val (jj));
+	      idx++;
+	    }
+	}
     }
   std::fill (non_local.row_ptr.begin () + idr,
              non_local.row_ptr.end (),
-             non_local.col_ind.size ());
+             idx);
 }
 
+
+void
+distributed_sparse_matrix::non_local_csr_update ()
+{
+
+  int idx = 0;
+  for (int i = 1; i < non_local.row_ptr.size (); ++i)
+    for (int j = 0; j < non_local.row_ptr[i] - non_local.row_ptr[i-1]; ++j)
+      {
+	non_local.a[idx] = (*this)[i-1][non_local.col_ind[idx]];
+	idx ++;
+      }
+}
 void
 distributed_sparse_matrix::remap ()
 {
@@ -111,7 +133,7 @@ distributed_sparse_matrix::assemble ()
                      ranges[ii+1] - ranges[ii] + 1,
                      MPI_INT, ii, send_tag, comm, &(reqs.back ()));
         }     
-      }
+    }
   
   MPI_Waitall (reqs.size (), &(reqs[0]), MPI_STATUSES_IGNORE);
   reqs.clear ();
@@ -138,7 +160,7 @@ distributed_sparse_matrix::assemble ()
                      - non_local.row_ptr[ranges[ii]],
                      MPI_INT, ii, send_tag, comm, &(reqs.back ()));
         }     
-      }
+    }
   MPI_Waitall (reqs.size (), &(reqs[0]), MPI_STATUSES_IGNORE);
   reqs.clear ();
     
@@ -163,10 +185,10 @@ distributed_sparse_matrix::assemble ()
                      - non_local.row_ptr[ranges[ii]],
                      MPI_DOUBLE, ii, send_tag, comm, &(reqs.back ()));
         }     
-      }
+    }
   MPI_Waitall (reqs.size (), &(reqs[0]), MPI_STATUSES_IGNORE);
   reqs.clear ();
-  
+
   /// 4) insert communicated values into sparse_matrix
   for (int ii = 0; ii < mpisize; ++ii) // loop over ranks
     if ((rank_nnz[ii] > 0) && (ii != mpirank))
@@ -179,14 +201,82 @@ distributed_sparse_matrix::assemble ()
           (*this)[jj + is][col_buffers[ii][kk]]
             += val_buffers[ii][kk];
 
-  /// 5) zero out communicated values  
-  for (int irow = 0; irow < is; ++irow)
-    if ((*this)[irow].size ())
-      for (auto& jj : (*this)[irow])
-        jj.second = 0.0;
 
-  for (int irow = ie; irow < this->size (); ++irow)
+  /// 5) zero out communicated values  
+  /*
+    for (int irow = 0; irow < is; ++irow)
     if ((*this)[irow].size ())
-      for (auto& jj : (*this)[irow])
-        jj.second = 0.0;
+    for (auto& jj : (*this)[irow])
+    jj.second = 0.0;
+
+    for (int irow = ie; irow < this->size (); ++irow)
+    if ((*this)[irow].size ())
+    for (auto& jj : (*this)[irow])
+    jj.second = 0.0;
+  */
+  nnz_local = 0;
+  for (int irow = is; irow < ie; ++irow)
+    nnz_local += (*this)[irow].size ();
+
+}
+
+
+void
+distributed_sparse_matrix::update_assemble ()
+{
+  std::vector<MPI_Request> reqs;
+  
+  non_local_csr_update ();
+
+
+  /// 3) communicate values
+  for (int ii = 0; ii < mpisize; ++ii)
+    {
+      if (ii == mpirank) continue; // No communication to self!
+      if (rank_nnz[ii] > 0) // we must receive something from rank ii
+        {
+          int recv_tag = ii   + mpisize * mpirank;
+          reqs.resize (reqs.size () + 1);
+          MPI_Irecv (&(val_buffers[ii][0]), val_buffers[ii].size (),
+                     MPI_DOUBLE, ii, recv_tag, comm, &(reqs.back ()));
+        }
+      if (non_local.row_ptr[ranges[ii+1]]
+          > non_local.row_ptr[ranges[ii]]) // we must send something to rank ii
+        {
+          int send_tag = mpirank + mpisize * ii;
+          reqs.resize (reqs.size () + 1);
+          MPI_Isend (&(non_local.a[non_local.row_ptr[ranges[ii]]]),
+                     non_local.row_ptr[ranges[ii+1]]
+                     - non_local.row_ptr[ranges[ii]],
+                     MPI_DOUBLE, ii, send_tag, comm, &(reqs.back ()));
+        }     
+    }
+  MPI_Waitall (reqs.size (), &(reqs[0]), MPI_STATUSES_IGNORE);
+  reqs.clear ();
+
+  /// 4) insert communicated values into sparse_matrix
+  for (int ii = 0; ii < mpisize; ++ii) // loop over ranks
+    if ((rank_nnz[ii] > 0) && (ii != mpirank))
+      for (int jj = 0;
+           jj < ranges[mpirank+1] - ranges[mpirank];
+           ++jj) // loop over rows of rank
+        for (int kk = row_buffers[ii][jj] - row_buffers[ii][0];
+             kk < row_buffers[ii][jj+1]   - row_buffers[ii][0];
+             ++kk) // loop over columns of row
+          (*this)[jj + is][col_buffers[ii][kk]]
+            += val_buffers[ii][kk];
+  /*
+ /// 5) zero out communicated values  
+ for (int irow = 0; irow < is; ++irow)
+ if ((*this)[irow].size ())
+ for (auto& jj : (*this)[irow])
+ jj.second = 0.0;
+
+ for (int irow = ie; irow < this->size (); ++irow)
+ if ((*this)[irow].size ())
+ for (auto& jj : (*this)[irow])
+ jj.second = 0.0;
+
+  */
+
 }
