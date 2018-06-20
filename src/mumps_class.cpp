1ddb754db1a13bd086695a479ed0383d67bbf971
elauksap <pasquale.africa@libero.it>
2018-06-20 15:54:34 +0000

    Assemble and set global_rhs

--- src/mumps_class.cpp
+++ src/mumps_class.cpp
@@ -148,6 +148,12 @@ mumps::set_rhs_distributed (distributed_vector &rhs)
   
   for (int row = 0; row < rhs.get_owned_data ().size (); ++row)
     (*glob_rhs)[rhs.get_range_start () + row] = rhs.get_owned_data ()[row];
+  
+  glob_rhs->assemble (replace_op);
+  
+  // Set actual rhs.
+  if (rank == 0)
+    set_rhs (glob_rhs->get_owned_data ());
 }
 
 int
