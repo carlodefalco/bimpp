2260c394df9a6cfeeb4a2299b50e01ad73fa02e0
elauksap <pasquale.africa@libero.it>
2018-08-06 17:44:47 +0200

    Add cell fields export

--- script/m/export.m
+++ script/m/export.m
@@ -1,5 +1,5 @@
 clear all;
 clc;
 
-export_tmesh_data ('tumor_growth_u_', {'tumor_growth_u_'}, {'u'},
+export_tmesh_data ('tumor_growth_u_', {'tumor_growth_u_'}, {'u'}, {}, {},
                    "out", 0:10, 2);
--- script/m/export_tmesh_data.m
+++ script/m/export_tmesh_data.m
@@ -1,50 +1,59 @@
-function export_tmesh_data (msh_basename, data_basenames,
-                            data_names, out_name = "out",
+function export_tmesh_data (msh_basename,
+                            nodedata_basenames, nodedata_names,
+                            celldata_basenames, celldata_names,
+                            out_name = "out",
                             steps = 0:10, nprocs = 1)
 
-  nfields = numel (data_names);
+  nnodefields = numel (nodedata_names);
+  ncellfields = numel (celldata_names);
+  
   for step = steps
-    
     fprintf("*** Step %d ***\n", step + 1);
     t = [];
     p = [];
 
-    for ii = 1 : nfields
-      u{ii} = [];
+    for ii = 1 : nnodefields
+      n{ii} = [];
+    endfor
+
+    for ii = 1 : ncellfields
+      c{ii} = [];
     endfor
     
     for proc = 0 : nprocs - 1
-      
       fprintf("Reading and processing input file %d...\n", proc + 1);
       
-      filename1 = sprintf (msh_basename, step, proc);
-      load ([filename1 ".octbin.gz"]);
+      filename = sprintf (msh_basename, step, proc);
+      load ([filename ".octbin.gz"]);
       
       t = [t, msh.t + columns(p) + 1];
       p = [p, msh.p];
 
-      for ii = 1 : nfields
-        filename1 = sprintf(data_basenames{ii}, step, proc);
-        load ([filename1 ".octbin.gz"]);
-        u{ii} = [u{ii}; msh.f];
+      for ii = 1 : nnodefields
+        filename = sprintf (nodedata_basenames{ii}, step, proc);
+        load ([filename ".octbin.gz"]);
+        n{ii} = [n{ii}; msh.f];
       endfor
-      
+
+      for ii = 1 : ncellfields
+        filename = sprintf (nodedata_basenames{ii}, step, proc);
+        load ([filename ".octbin.gz"]);
+        c{ii} = [c{ii}; msh.f];
+      endfor
     endfor
     
     msh.p = p;
     msh.t = t;
     
     filename_out = sprintf ([out_name "_%d"], step);
-    if (exist([filename_out ".vtu"], "file"))
-      delete([filename_out ".vtu"]);
+    if (exist ([filename_out ".vtu"], "file"))
+      delete ([filename_out ".vtu"]);
     endif
 
     fpl_vtk_write_field_octree (filename_out, msh,
-                                [u; data_names].',
-                                {}, 1);
+                                [n; nodedata_names].',
+                                [c; celldata_names].', 1);
     
     fprintf("\n");
-
   endfor
-
 endfunction
