6e693983a11e32a306abe4e5c7985e7f0efa1ae6
Federica Filippini <federicafilippini@MBP-di-Federica.lan>
2019-02-22 11:53:43 +0100

    Aggiunto ordering come in tmesh.h

--- src/tmesh_3d.h
+++ src/tmesh_3d.h
@@ -22,6 +22,15 @@
 #include <array>
 #include <vector>
 
+/// Ordering.
+using ordering = std::function<size_t (p4est_gloidx_t)>;
+
+template<size_t ntot = 1, size_t n = 0>
+size_t
+dof_ordering (p4est_gloidx_t gt)
+{ return ntot*gt+n; }
+
+extern ordering default_ord;
 
 /// C++ interface class for p4est 3d quadrant meshes.
 class
