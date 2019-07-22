053d2e75f6b73b0019a24d7ae2cc2b8c5ef1fb71
Federica Filippini <federicafilippini@MacBook-Pro-di-Federica-2.local>
2019-07-22 16:41:42 +0200

    refactoring example examples/tmesh_3d/p4est_refine_box.cpp

--- examples/tmesh_3d/p4est_refine_box.cpp
+++ examples/tmesh_3d/p4est_refine_box.cpp
@@ -36,24 +36,36 @@ rectangle_list_refinement (tmesh_3d::quadrant_iterator quadrant,
 }
 
 
+/// main
 int main(int argc, char ** argv)
 {
   MPI_Init (&argc, &argv);
   
-  int      recursive, partforcoarsen, balance;
-  MPI_Comm mpicomm = MPI_COMM_WORLD;  							
-  int      rank, size;
-  tmesh_3d tmsh;
+  int       recursive, partforcoarsen, balance;
+  MPI_Comm  mpicomm = MPI_COMM_WORLD;  							
+  int       rank, size;
+  tmesh_3d  tmsh;
 
-  size_t num_quads = 0;			// number of quadrants
-  size_t nq = 0;						// index of current quadrant
-  
-  double local_h = 0;				// mesh size on current processor
-  double global_h = 0;			// global mesh size
-  
-  using q1_vec = q1_vec<distributed_vector>;
+  using q1_vec    = q1_vec<distributed_vector>;
   using gradient3 = gradient3<distributed_vector>;
-  using idx_t = tmesh_3d::idx_t;
+  using idx_t     = tmesh_3d::idx_t; 
+
+  unsigned nref_1 = 1;                // number of initial uniform refinements
+  unsigned nref_2 = 4;                // number of iterative refinements
+
+  // Mesh parameters
+  std::vector<idx_t> nnodes (nref_2, 0);      // number of nodes
+  std::vector<double> h_step (nref_2, 0.);    // mesh size
+
+  // Errors at every step
+  std::vector<double> error (nref_2,0.);      // ||u - u_ex||_L^2(q)
+  std::vector<double> errorH1 (nref_2,0.);    // |u - u_ex|_H^1(q)
+  std::vector<double> errorStar (nref_2,0.);  // ||u_star - u_ex||_L^2(q)
+  std::vector<double> errorH1Star(nref_2,0.); // ||du_star - grad(u_ex)||_L^2(q)
+
+  // Estimators at every step
+  std::vector<double> estSol (nref_2,0.);     // ||u^* - u||_L^2(q)
+  std::vector<double> estGrad (nref_2,0.);    // ||grad^* u - grad u||_L^2(q)
 
   MPI_Comm_rank (mpicomm, &rank);
   MPI_Comm_size (mpicomm, &size);
@@ -61,16 +73,6 @@ int main(int argc, char ** argv)
   MPI_Barrier (MPI_COMM_WORLD); { if (rank == 0) tic (); }
   
   // Create mesh.
-  /*
-  std::vector<double> p = {0, 0, 0,
-                           0, 0, 1,
-                           0, 1, 0,
-                           0, 1, 1,
-                           1, 0, 0,
-                           1, 0, 1,
-                           1, 1, 0,
-                           1, 1, 1};
-  */
   std::vector<double> p = {0, 0, 0,
                            1, 0, 0,
                            0, 1, 0,
@@ -85,268 +87,241 @@ int main(int argc, char ** argv)
 
   double L = 1., h = 1./5.;
   
+  // Define function for rectangle refinement.
   std::function<int (tmesh_3d::quadrant_iterator)> box_refinement =
     [L,h] (tmesh_3d::quadrant_iterator qi)
     { return rectangle_list_refinement(qi, L, h); };
 
   if (rank == 0) { toc ("*** Initialization ***"); }
 
-  // Definition of exact solution
+  // Definition of exact solution.
   func3 u_ex = 
     [L] (double x, double y, double z) -> double
     {
       return (sin(2*M_PI*x/L) * cos(2*M_PI*y/L) * sin(2*M_PI*z/L));
-      //return (5*x + 4*y + 2*z);
     };
 
-  // Definition of exact derivatives
+  // Definition of exact derivatives.
   func3 du_x_ex =
     [L] (double x, double y, double z) -> double
     {
-      return (2*M_PI * cos(2*M_PI*x/L) * cos(2*M_PI*y/L) * sin(2*M_PI*z/L) / L);
-      //return 5.;
+      return (2*M_PI *cos(2*M_PI*x/L) *cos(2*M_PI*y/L) *sin(2*M_PI*z/L) /L);
     };
 
   func3 du_y_ex =
     [L] (double x, double y, double z) -> double
     {
-      return (-2*M_PI * sin(2*M_PI*x/L) * sin(2*M_PI*y/L) * sin(2*M_PI*z/L) /L);
-      //return 4.;
+      return (-2*M_PI *sin(2*M_PI*x/L) *sin(2*M_PI*y/L) *sin(2*M_PI*z/L) /L);
     };
 
   func3 du_z_ex =
     [L] (double x, double y, double z) -> double
     {
-      return (2*M_PI * sin(2*M_PI*x/L) * cos(2*M_PI*y/L) * cos(2*M_PI*z/L) / L);
-      //return 2.;
+      return (2*M_PI *sin(2*M_PI*x/L) *cos(2*M_PI*y/L) *cos(2*M_PI*z/L) /L);
     };
 
-  // Containers for errors
-  std::array<double,4> loc_err;			// loc_err[0] -> error
-  																	// loc_err[1] -> error_du
-  																	// loc_err[2] -> error_star
-    																// loc_err[3] -> error_star_du
-
-  std::array<double,4> errors;			// errors[0] -> errors
-  																	// errors[1] -> errors_du
-  																	// errors[2] -> errors_star
-    																// errors[3] -> errors_star_du
-
-  std::vector<idx_t> nnodes;
-  std::vector<idx_t> nquads;
-  std::vector<double> h_vec;
-
-  // Uniform refinement.
   recursive = 0;
   partforcoarsen = 1;
+
+  // Initial uniform refinement.
+  for (int cycle = 0; cycle < nref_1; ++cycle)
+    {
+      // Refine mesh.
+      MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
+      tmsh.set_refine_marker (uniform_refinement);
+      tmsh.refine(recursive, partforcoarsen);        
+      if (rank == 0) 
+        { 
+          sprintf (step, "*** Refinement and balancing %3.3d ***", 
+                    cycle);
+          toc (step); 
+        }
+
+      // Export refined mesh.
+      MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
+      tmsh.vtk_export ((std::string("p4est_refine_box_inital_mesh_")
+                          + std::to_string(cycle)).c_str());
+      if (rank == 0) { toc ("*** Export initial mesh ***"); }
+    } 
   
-  int file_number = 0, refine_number = 0;
-  for (int cycle = 0; cycle < 1; ++cycle)
+  // Iterative refinement according to boxes.  
+  for (int cycle = 0; cycle < nref_2; ++cycle)
     {
-        MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
-        tmsh.set_refine_marker (uniform_refinement);
-        tmsh.refine(recursive, partforcoarsen);        
-        if (rank == 0) 
-          { 
-             sprintf (step, "*** Refinement and balancing %3.3d ***", 
-                      refine_number++);
-             toc (step); 
-          }
-
-        MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
-        sprintf(name,"p4est_refine_box_%4.4d",file_number++);
-        tmsh.vtk_export (name);
-        if (rank == 0) { toc ("*** Export ***"); }
-    }
-
-  // *************************************************************************
-
-  // definition of u (uniform refined mesh)
-  q1_vec u_vec(tmsh.num_owned_nodes());
-  bim3a_solution_with_ghosts(tmsh,u_vec);
-  for (auto q = tmsh.begin_quadrant_sweep();
-            q != tmsh.end_quadrant_sweep();
-            ++q)
-    {
-      for (int nn = 0; nn < 8; ++nn)
-        {
-          // assemble non-hanging nodes
-          if (! q->is_hanging(nn))
-            u_vec[q->gt(nn)] = u_ex(q->p(0,nn), q->p(1,nn), q->p(2,nn));
-        }  
-    }
-  u_vec.assemble(replace_op);
-
-  // Compute gradient (uniform refined mesh)
-  gradient3 grad_star = bim3c_quadtree_pde_recovered_gradient(tmsh,u_vec);
-
-  // Compute recovered solution (uniform refined mesh)
-  q2_vec3 u_star = bim3c_quadtree_pde_recovered_solution(tmsh,u_vec,
-  																												grad_star);
-
-  // Compute h and error norms (uniform refined mesh)
-  num_quads = tmsh.num_global_quadrants();
-  local_h = std::numeric_limits<double>::max ();
-  global_h = 0;
-  loc_err.fill(0.);
-  errors.fill(0.);
-
-  for (auto q = tmsh.begin_quadrant_sweep();
-            q != tmsh.end_quadrant_sweep();
-            ++q)
-    {
-    	double hx = q->p(0, 7) - q->p(0, 0);
-      double hy = q->p(1, 7) - q->p(1, 0);
-      double hz = q->p(2, 7) - q->p(2, 0);
-      local_h = std::min(local_h, std::sqrt(hx*hx + hy*hy + hz*hz));
-
-      // || u_k - u_ex ||_{L^2}
-      loc_err[0] += std::pow(l2_error(q, u_ex, u_vec), 2);
-      // || u^*_k - u_ex ||_{L^2}
-      loc_err[1] += std::pow(l2_star_error(q, u_ex, u_star), 2);
-      // | sigma - grad(u_ex) |_{H^1}
-      loc_err[2] += std::pow(semih1_error(q, du_x_ex, du_y_ex, du_z_ex, 
-      																		u_vec), 2);
-      // | sigma^* - grad(u_ex) |_{H^1}
-      loc_err[3] += std::pow(semih1_star_error(q, du_x_ex,du_y_ex,du_z_ex, 
-      																					grad_star), 2);
-    }
-
-  MPI_Reduce(&local_h, &global_h, 1, MPI_DOUBLE, MPI_MIN, 0, mpicomm);
-
-  MPI_Reduce(loc_err.data(), errors.data(), 4, MPI_DOUBLE, MPI_SUM,
-  						0, mpicomm);
-
-  for (double& d : errors)
-  	d = std::sqrt(d);
-
-  nnodes.push_back(tmsh.num_global_nodes());
-  nquads.push_back(num_quads);
-  h_vec.push_back(global_h);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  if (rank == 0)
-    {
-    	std::cout << "nnodes = " << nnodes[nnodes.size()-1] << std::endl;
-    	std::cout << "nquads = " << nquads[nquads.size()-1] << std::endl;
-    	std::cout << "h = " << h_vec[h_vec.size()-1] << std::endl;
-      for (unsigned j=0; j<4; ++j)
-        std::cout << "err" << j << " = " << errors[j] << std::endl;
-      std::cout << std::endl;
-    }
-
-  // *************************************************************************  
-  
-  // Refine according to boxes.  
-  for (int cycle = 0; cycle < 4; ++cycle)
-    {
-      // Adaptive refinement.
       MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
 
-      tmsh.set_refine_marker (uniform_refinement); 
+      tmsh.set_refine_marker (uniform_refinement);  // uniform refinement
       tmsh.refine (recursive, partforcoarsen);
       
-      tmsh.set_refine_marker (box_refinement);
+      tmsh.set_refine_marker (box_refinement);      // rectangle refinement
       tmsh.refine (recursive, partforcoarsen);
       
       if (rank == 0) 
-          { 
-             sprintf (step, "*** Refinement and balancing %3.3d ***", 
-                      refine_number++);
-             toc (step); 
-          }
+        { 
+          sprintf (step, "*** Refinement and balancing %3.3d ***", 
+                    (cycle+nref_1));
+          toc (step); 
+        }
 
-        // *******************************************************************
-
-        // definition of u (adaptive refined mesh)
-        q1_vec u_vec_1(tmsh.num_owned_nodes());
-        bim3a_solution_with_ghosts(tmsh,u_vec_1);
-        for (auto q = tmsh.begin_quadrant_sweep();
-                  q != tmsh.end_quadrant_sweep();
-                  ++q)
-          {
-            for (int nn = 0; nn < 8; ++nn)
-              {
-                // assemble non-hanging nodes
-                if (! q->is_hanging(nn))
-                  u_vec_1[q->gt(nn)] = u_ex(q->p(0,nn), q->p(1,nn), 
-                                            q->p(2,nn));
-              }  
-          }
-        u_vec_1.assemble(replace_op);
-
-        // gradient (adaptive refined mesh)
-        gradient3 grad_star_1 = bim3c_quadtree_pde_recovered_gradient(tmsh,
-                                                                      u_vec_1);
-
-        // recovered solution (adaptive refined mesh)
-        q2_vec3 u_star_1 = bim3c_quadtree_pde_recovered_solution(tmsh,u_vec_1,
-                                                                  grad_star_1);
-
-        // Compute h and error norms (adaptive refined mesh)
-			  num_quads = tmsh.num_global_quadrants();
-			  local_h = std::numeric_limits<double>::max ();
-			  global_h = 0;
-			  loc_err.fill(0.);
-			  errors.fill(0.);
-
-        for (auto q = tmsh.begin_quadrant_sweep();
-            q != tmsh.end_quadrant_sweep();
-            ++q)
-			    {
-			    	double hx = q->p(0, 7) - q->p(0, 0);
-			      double hy = q->p(1, 7) - q->p(1, 0);
-			      double hz = q->p(2, 7) - q->p(2, 0);
-			      local_h = std::min(local_h, std::sqrt(hx*hx + hy*hy + hz*hz));
-
-			      // || u_k - u_ex ||_{L^2}
-			      loc_err[0] += std::pow(l2_error(q, u_ex, u_vec_1), 2);
-			      // || u^*_k - u_ex ||_{L^2}
-			      loc_err[1] += std::pow(l2_star_error(q, u_ex, u_star_1), 2);
-			      // | sigma - grad(u_ex) |_{H^1}
-			      loc_err[2] += std::pow(semih1_error(q, du_x_ex, du_y_ex, du_z_ex, 
-			      																		u_vec_1), 2);
-			      // | sigma^* - grad(u_ex) |_{H^1}
-			      loc_err[3] += std::pow(semih1_star_error(q, du_x_ex,du_y_ex,du_z_ex, 
-			      																					grad_star_1), 2);
-			    }
-
-			  MPI_Reduce(&local_h, &global_h, 1, MPI_DOUBLE, MPI_MIN, 0, mpicomm);
-
-			  MPI_Reduce(loc_err.data(), errors.data(), 4, MPI_DOUBLE, MPI_SUM,
-			  						0, mpicomm);
-
-			  for (double& d : errors)
-			  	d = std::sqrt(d);
-
-			  nnodes.push_back(tmsh.num_global_nodes());
-			  nquads.push_back(num_quads);
-			  h_vec.push_back(global_h);
-
-			  MPI_Barrier(MPI_COMM_WORLD);
-			  if (rank == 0)
-			    {
-			    	std::cout << "nnodes = " << nnodes[nnodes.size()-1] << std::endl;
-			    	std::cout << "nquads = " << nquads[nquads.size()-1] << std::endl;
-			    	std::cout << "h = " << h_vec[h_vec.size()-1] << std::endl;
-			      for (unsigned j=0; j<4; ++j)
-			        std::cout << "err" << j << " = " << errors[j] << std::endl;
-			      std::cout << std::endl;
-			    }
-
-      	// ******************************************************************* 
-
-      // Export mesh.
-      MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }      
-      sprintf(name,"p4est_refine_box_%4.4d",file_number++);
-      tmsh.vtk_export (name);
+      // Export refined mesh.
+      MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
+      tmsh.vtk_export ((std::string("p4est_refine_box_mesh_")
+                          + std::to_string(cycle)).c_str());
       if (rank == 0) { toc ("*** Export ***"); }
 
+      // Compute number of nodes.
+      nnodes[cycle] = tmsh.num_global_nodes();
+
+      // Definition of u.
+      q1_vec u_vec (tmsh.num_owned_nodes());
+      bim3a_solution_with_ghosts (tmsh,u_vec);
+      for (auto q = tmsh.begin_quadrant_sweep();
+                q != tmsh.end_quadrant_sweep();
+                ++q)
+        {
+          for (int nn = 0; nn < 8; ++nn)
+            {
+              // assemble non-hanging nodes
+              if (! q->is_hanging(nn))
+                u_vec[q->gt(nn)] = u_ex(q->p(0,nn), q->p(1,nn), q->p(2,nn));
+            }  
+        }
+      u_vec.assemble(replace_op);
+
+      // Compute recovered gradient.
+      gradient3 grad_star = bim3c_quadtree_pde_recovered_gradient (tmsh,
+                                                                    u_vec);
+
+      // Compute recovered solution.
+      q2_vec3 u_star = bim3c_quadtree_pde_recovered_solution(tmsh,u_vec,
+                                                              grad_star);
+
+      // Export solution.
+      tmsh.octbin_export((std::string("p4est_refine_box_u_")
+                          + std::to_string(cycle)).c_str(), 
+                          u_vec);
+
+      // Compute mesh size, errors and estimators.
+      MPI_Barrier (MPI_COMM_WORLD); if (rank == 0) { tic (); }
+      double  hx = 0, hy = 0, hz = 0, h = 0;
+
+      double err = 0.0;
+      double errH1 = 0.0;
+      double errstar = 0.0;
+      double errH1star = 0.0;
+      double estsol = 0.0;
+      double estgrad = 0.0;
+
+      std::vector<double> sol_est (tmsh.num_local_quadrants());
+      std::vector<double> grad_est (tmsh.num_local_quadrants());
+
+      for (auto q = tmsh.begin_quadrant_sweep();
+          q != tmsh.end_quadrant_sweep();
+          ++q)
+        {
+          // mesh size
+          double hx = q->p(0, 7) - q->p(0, 0);
+          double hy = q->p(1, 7) - q->p(1, 0);
+          double hz = q->p(2, 7) - q->p(2, 0);
+          h = std::max(h, std::sqrt(hx*hx + hy*hy + hz*hz));
+
+          // ||u - u_ex||_L^2(q)
+          err += std::pow(l2_error(q, u_ex, u_vec), 2);
+
+          // ||u_star - u_ex||_L^2(q)
+          errstar += std::pow(l2_star_error(q, u_ex, u_star), 2);
+
+          // |u - u_ex|_H^1(q)
+          errH1 += std::pow(semih1_error(q, du_x_ex, du_y_ex, du_z_ex, 
+                                          u_vec), 2);
+
+          // ||du_star - grad(u_ex)||_L^2(q)
+          errH1star = std::pow(semih1_star_error(q, du_x_ex,du_y_ex,du_z_ex,
+                                                  grad_star), 2);
+
+          double temp_est = 0.;
+
+          // ||u^* - u||_L^2(q)
+          temp_est = std::pow(estimator_sol(q, u_star, u_vec), 2); 
+          estsol += temp_est;
+          sol_est[q->get_forest_quad_idx ()] = temp_est;
+
+          // ||grad^* u - grad u||_L^2(q)
+          temp_est = std::pow(estimator_grad(q, grad_star, u_vec), 2);
+          estgrad += temp_est;
+          grad_est[q->get_forest_quad_idx ()] = temp_est;
+        }
+
+      // Export estimators.
+      tmsh.octbin_export_quadrant ((std::string("p4est_refine_box_err_")
+                                      + std::to_string(cycle)).c_str(), 
+                                      sol_est);
+      tmsh.octbin_export_quadrant ((std::string("p4est_refine_box_err_")
+                                      + std::to_string(cycle)).c_str(), 
+                                      grad_est);
+
+      // Global mesh size.
+      MPI_Reduce(&h, &h_step[cycle], 1, MPI_DOUBLE, MPI_MAX, 0, mpicomm);
+
+      // Global errors
+      //
+      // ||u - u_ex||_L^2(q)
+      MPI_Reduce (&err, &error[cycle], 1, MPI_DOUBLE, MPI_SUM, 0, mpicomm);
+      error[cycle] = std::sqrt(error[cycle]);
+      //
+      // |u - u_ex|_H^1(q)
+      MPI_Reduce (&errH1, &errorH1[cycle], 1, MPI_DOUBLE, MPI_SUM, 0, mpicomm);
+      errorH1[cycle] = std::sqrt(errorH1[cycle]);
+      //
+      // ||u_star - u_ex||_L^2(q)
+      MPI_Reduce (&errstar, &errorStar[cycle], 1, MPI_DOUBLE, MPI_SUM, 
+                  0, mpicomm);
+      errorStar[cycle] = std::sqrt(errorStar[cycle]);
+      //
+      // ||du_star - grad(u_ex)||_L^2(q)
+      MPI_Reduce (&errH1star, &errorH1Star[cycle], 1, MPI_DOUBLE, MPI_SUM, 
+                  0, mpicomm);
+      errorH1Star[cycle] = std::sqrt(errorH1Star[cycle]);
+
+      // Global estimators
+      //
+      // ||u^* - u||_L^2(q)
+      MPI_Reduce (&estsol, &estSol[cycle], 1, MPI_DOUBLE, MPI_SUM, 0, mpicomm);
+      estSol[cycle] = std::sqrt(estSol[cycle]);
+      //
+      // ||grad^* u - grad u||_L^2(q)
+      MPI_Reduce (&estgrad, &estGrad[cycle], 1, MPI_DOUBLE, MPI_SUM, 0, 
+                  mpicomm);
+      estGrad[cycle] = std::sqrt(estGrad[cycle]);
+
+      MPI_Barrier (MPI_COMM_WORLD); 
+      if (rank == 0) 
+        toc ("Compute h and error ");
     }
 
   MPI_Barrier (MPI_COMM_WORLD);
-  if (rank == 0) { print_timing_report (); }
+  if (rank == 0) 
+    { 
+      // timing report
+      print_timing_report ();
+
+      // mesh size, errors and estimators
+      for (unsigned step = 0; step < nnodes.size(); ++step)
+        {
+          std::cout << "\nStep " << step << ", #nodes: "
+                    << nnodes[step] << ", h: "
+                    << h_step[step] << std::endl;
+          std::cout << "\tL2 norm = " << error[step] 
+                    << "\n\tH1 seminorm = " << errorH1[step] 
+                    << "\n\tL2* norm = " << errorStar[step] 
+                    << "\n\tL2* norm (gradient) = " << errorH1Star[step]
+                    << "\n\tSolution estimator = " << estSol[step]
+                    << "\n\tGradient estimator = " << estGrad[step]
+                    << std::endl;
+          std::cout << std::endl;
+        }
+    }
   
   MPI_Finalize ();
   return 0;
 }
+
