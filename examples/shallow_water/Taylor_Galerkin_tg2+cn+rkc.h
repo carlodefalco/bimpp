b3e37d86eed96c4dad737756e1f533d265662910
federico gatti <federicog@federicos-MacBook-Pro.local>
2021-12-21 15:43:25 +0100

    draft tg2+cn+rkc

---
+++ examples/shallow_water/Taylor_Galerkin_tg2+cn+rkc.h
@@ -0,0 +1,226 @@
+#ifndef TAYLOR_GALERKIN_H
+#define TAYLOR_GALERKIN_H
+
+#include <numeric>
+#include <bim_distributed_vector.h>
+#include <tmesh.h>
+#include <quad_operators.h>
+
+ 
+
+
+class TG2_scheme 
+{
+  using Q1  = q1_vec<distributed_vector>;
+  using Q0  = std::vector<double>;
+  
+public:
+  
+  TG2_scheme(const Q1& sol,
+             const Q1& sold,
+             const Q1& soldd,
+             Q1& incr,
+             Q1& incr_source,
+             std::vector<std::array<double,4>>& incr_anti_diff,
+             Q1& P_plus,
+             Q1& P_minus,
+             Q0& sol_onehalf,
+             const Q1& mass,
+             const ordering& oh,
+             const ordering& oUx, 
+             const ordering& oUy, 
+             const Q1& Z,
+             const std::vector<double>& nodal_error,
+             const Q0& slope_x,
+             const Q0& slope_y,
+             const double& DELTAT,
+             const double& h_min,
+             const bool& is_non_reflBC,
+             const bool& is_bed_friction,
+             const bool& is_stress_tensor,
+             const double& grav,
+             const double& density,
+             const double& turbulence_coeff,
+             const double& surface_pressure,
+             const double& bed_friction_angle_rad,
+             const double& fluid_viscosity,
+             const double& yield_shear_stress);
+  
+  TG2_scheme() = delete;
+  
+  ~TG2_scheme() = default;
+  
+  
+  void
+  compute_dt (tmesh::quadrant_iterator quadrant);
+  
+  void
+  compute_dt_adaptive (tmesh::quadrant_iterator quadrant);
+  
+  void
+  first_step (tmesh::quadrant_iterator quadrant);
+
+  void
+  compute_nodal_anti_diffusive_fluxes (tmesh::quadrant_iterator quadrant);
+
+  void
+  loop_step (tmesh::quadrant_iterator quadrant);
+  
+  void
+  second_step (tmesh::quadrant_iterator quadrant);
+  
+  void
+  flux_limiter(const double& Q_min, const double& Q_max, const double& Q_dof, const double& P_plus_Q, const double& P_minus_Q, const double& flux_on_the_node, const double& mass_node, double& phi_cell_Q);
+  
+  
+  void
+  set_dt (const double dt_);
+  
+  void
+  set_times(const double& time, const double& time_old, const double& time_oldd);
+  
+  double
+  get_dt ();
+  
+  
+  double dt;
+  
+  double Dx, Dy, area;
+  
+  
+    ///  quadrant vertex (dofs) coordinates
+    ///  The assumed numbering for quadrant nodes is
+    ///  the following :
+    ///    ^
+    ///   yI
+    ///   2------------------3
+    ///   |                  |
+    ///   |                  |
+    ///   |                  |
+    ///   |                  |
+    ///   0------------------1 -->x
+  
+  std::array<double, 4> xn = {0, 0, 0, 0};
+  std::array<double, 4> yn = {0, 0, 0, 0};
+  
+  // local dofs for state vector components
+  std::array<double, 4> hdof    = {0, 0, 0, 0};
+  std::array<double, 4> Uxdof   = {0, 0, 0, 0};
+  std::array<double, 4> Uydof   = {0, 0, 0, 0};
+  std::array<double, 4> Z_node  = {0, 0, 0, 0};
+  std::array<double, 4> P_plus_h_dof   = {0, 0, 0, 0};
+  std::array<double, 4> P_minus_h_dof  = {0, 0, 0, 0};
+  std::array<double, 4> P_plus_Ux_dof  = {0, 0, 0, 0};
+  std::array<double, 4> P_minus_Ux_dof = {0, 0, 0, 0};
+  std::array<double, 4> P_plus_Uy_dof  = {0, 0, 0, 0};
+  std::array<double, 4> P_minus_Uy_dof = {0, 0, 0, 0};
+  
+  // std::array<double, 4> source_h_node  = {0, 0, 0, 0};
+  // std::array<double, 4> source_Ux_node = {0, 0, 0, 0};
+  // std::array<double, 4> source_Uy_node = {0, 0, 0, 0};
+  
+  std::array<double, 4> fluxx_h_node    = {0, 0, 0, 0}, fluxy_h_node    = {0, 0, 0, 0};
+  std::array<double, 4> fluxx_Ux_node   = {0, 0, 0, 0}, fluxy_Ux_node   = {0, 0, 0, 0};
+  std::array<double, 4> fluxx_Uy_node   = {0, 0, 0, 0}, fluxy_Uy_node   = {0, 0, 0, 0};
+  
+  
+  std::array<double, 3> sigma_stress = {0., 0., 0.};
+  
+  
+  // flux functions
+  double
+  h_flux_formula_x (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  h_flux_formula_y (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Ux_flux_formula_x (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Ux_flux_formula_y (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Uy_flux_formula_x (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Uy_flux_formula_y (const double& h, const double& Ux, const double& Uy);
+
+
+  // stress functions
+  double
+  Ux_stress_formula_x (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Ux_stress_formula_y (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Uy_stress_formula_x (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Uy_stress_formula_y (const double& h, const double& Ux, const double& Uy);
+
+
+  std::array<double,3>
+  compute_nodal_stress (const double& h, const double& Ux, const double& Uy, const std::array<double,2>& grad_cell_ux, const std::array<double,2>& grad_cell_uy);
+
+  std::array<double,6>
+  compute_nodal_def_grad (const double& h, const double& Ux, const double& Uy, const std::array<double,2>& grad_cell_ux, const std::array<double,2>& grad_cell_uy);
+
+
+  
+  // source terms
+  double
+  h_src_formula (const double& h, const double& Ux, const double& Uy);
+  
+  double
+  Ux_src_formula (const double& h, const double& Ux, const double& Uy, const double& dZdx);
+  
+  double
+  Uy_src_formula (const double& h, const double& Ux, const double& Uy, const double& dZdy);
+  
+  double time, timed, timedd;
+  double nu_htot = 0.;
+  
+  const Q1& sol;
+  const Q1& sold;
+  const Q1& soldd;
+  Q1& incr;
+  Q1& incr_source;
+  std::vector<std::array<double,4>>& incr_anti_diff;
+  Q1& P_plus;
+  Q1& P_minus;
+  Q0& sol_onehalf;
+  const Q1& Z;
+  const std::vector<double>& nodal_error;
+  const Q0& slope_x;
+  const Q0& slope_y;
+  const Q1& mass;
+  
+private:
+
+  std::array<double, 4> vel_rusanov_x, vel_rusanov_y, isdof_or_hanging, der_coeffs_x, der_coeffs_y;
+  std::array<double, 2> grad_cell_h, grad_cell_Ux, grad_cell_Uy, grad_cell_ux, grad_cell_uy;
+  
+  const ordering& ordh;
+  const ordering& ordUx;
+  const ordering& ordUy;
+  const double& DELTAT;
+  const double& epsilon;
+  const bool& is_non_reflBC;
+  const bool& is_bed_friction;
+  const bool& is_stress_tensor;
+  const double& grav;
+  const double& density;
+  const double& turbulence_coeff;
+  const double& surface_pressure;
+  const double& bed_friction_angle_rad;
+  const double& fluid_viscosity;
+  const double& yield_shear_stress;
+  
+};
+
+
+
+
+#endif
