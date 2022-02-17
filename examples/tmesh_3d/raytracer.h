2e38d67c8eb7457c1874254df89cff9b468e6c17
martinapoliti <martina.politi@mail.polimi.it>
2022-02-17 17:08:46 +0100

    added ray_cache struct

--- examples/tmesh_3d/raytracer.h
+++ examples/tmesh_3d/raytracer.h
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <vector>
+#include <array>
 #include <nanoshaper.h>
 
 using int_coord_t = unsigned long long int;
@@ -10,69 +11,73 @@ using int_coord_t = unsigned long long int;
 struct
 crossings_t {
 
-   /*struct
-   vector3d {
-     double x;
-     double y;
-     double z;
-   };*/
-
-   double start[3],  end; //start and end point of the choosen direction
-   //bool   startv, endv; //??
-
-   //std::vector<double>   params; 
+   static double start, end; //start and end point of the choosen direction (l_c[1], r_c[1])
+   static NS::NanoShaper ns;
+   
+   double point[2]; //point x and z coords: the ones that prescribe the ray
+ 
    std::vector<bool>     flags; //in or out (in=1, out=0) ex: if flags[1]==1 -> [inters[1].first ; inters[2].first] is IN  
-   //std::vector<vector3d> normals; //vector of normals 
-   
-   std::vector<std::pair<double,double*> > inters; //intersections and normals
-   
-   crossings_t (double start_x, double start_y, double start_z, double end_, NS::NanoShaper & ns_)
-    : end(end_)
-    {
-      start[0] = start_x;
-      start[1] = start_y;
-      start[2] = start_z;
-      computeIntersections (ns_);
-    };
+   std::vector<std::pair<double,double*>> inters; //intersections and normals
    
    void
-   computeIntersections (NS::NanoShaper & ns); //it fills inters vector
+   compute_intersections (); //it fills inters vector
    
    double
-   is_inside_molecule(double point); //given a point coord it returns 1 if the point is inside the molecule, 0 otherwise
+   is_inside_molecule (double y);
 
 };
 
-/*
+struct
+map_compare
+{
+  bool operator ()(const std::array<double, 2> & arr1, const std::array<double, 2> & arr2) const
+  {  
+    if (arr1.at(0) < arr2.at(0))
+      return 1;
+      
+    else if (arr1.at(0) == arr2.at(0))
+    {
+      if (arr1.at(1) < arr2.at(1))
+        return 1;
+      else 
+        return 0;
+    }
+    
+    else 
+      return 0;
+  }
+};
+
 struct
 ray_cache_t 
-   {
-   std::map<int_coord_t, std::map<int_coord_t, crossings_t>> rays;
+{
+   std::map<std::array<double, 2> , crossings_t, map_compare> rays; //x-coord and z-coord, correspondent ray
    
-   crossings_t &
-   operator() (int_coord_t x0, int_coord_t x1) 
+   crossings_t 
+   operator() (double x0, double x1) 
    {
      static crossings_t dummy;
-     crossings_t & tmp = dummy;
-     auto it0 = rays.find (x0);
+     crossings_t & cr_t = dummy;
      
-     if (it0 != rays.end ()) //if x0 is present in rays
-     {
-       auto it1 = it0->second.find (x1); //it1 is the map with x1 as first elem
-       if (it1 != it0->second.end ())  //if it1 exists
-         tmp = it1->second; //temp is crossings_t of it1
-     }
+     static std::array<double, 2> start_point;
+     start_point = {x0, x1};
+     auto it0 = rays.find (start_point);
+     
+     if (it0 != rays.end ())
+       cr_t = it0->second;
       
-     else //x0 not in rays 
+     else
      {
        // create a new ray
        crossings_t tmp;
-       rays[x0][x1] = tmp; //create a map element with first elem x0, then the second element is a map with x1 as first, tmp as second elem
-       tmp = rays[x0][x1];
+       tmp.point[0] = x0;
+       tmp.point[1] = x1;
+       tmp.compute_intersections ();
+       rays[start_point] = tmp;         
+       cr_t = rays[start_point];
      }
-     return tmp;
+     
+     return cr_t;
    }
-   
-};*/
-
+};
 #endif //RAYTRACER_H
