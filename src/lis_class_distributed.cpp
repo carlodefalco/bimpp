89fcc33115e3757b2e7f32a4e2109f2994a3af7d
Carlo de Falco <carlo.defalco@polimi.it>
2022-11-07 13:12:09 +0100

    lis distributed class

---
+++ src/lis_class_distributed.cpp
@@ -0,0 +1,267 @@
+/*
+  Copyright (C) 2011 Carlo de Falco
+  This software is distributed under the terms
+  the terms of the GNU/GPL licence v3
+*/
+/*! \file lis_class.cpp
+  \brief interface for linear solver built with for lis library.
+*/
+
+#include "lis_class.h"
+#include <cstdlib>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+
+/*
+ 
+  Typical usage :
+
+  linear_solver *solver = new lis ();
+  solver->set_lhs_structure (lhs.num_owned_rows (), ir, jc);
+  solver->analyze ();
+  solver->set_lhs_data (xa);
+  solver->factorize ();
+  solver->set_rhs (rhs);
+  if (solver->solver_type () == "iterative")
+  solver->set_max_iterations (1000);
+  solver->set_tolerance (1e-12);
+  solver->solve ();
+  solver->cleanup ();
+
+
+*/
+
+int num = 0;
+
+int
+lis::init_lis_objects ()
+{
+  // Build lis structures
+  row = new LIS_INT[n + 1];
+  col = new LIS_INT[nnz];
+  value = new LIS_SCALAR[nnz];
+
+  for (int i = 0; i < nnz ; ++i)
+    col[i] = jcol[i] - index_base;
+   
+  for (int i = 0; i < n + 1; ++i)
+    row[i] = row_ptr[i] - row_ptr[0];
+
+  if (initialized)
+    {
+      destroy_lis_objects ();
+      initialized = false;
+    }
+
+  lis_matrix_create (MPI_COMM_WORLD, &A);
+  lis_vector_create (MPI_COMM_WORLD, &b);
+  lis_vector_create (MPI_COMM_WORLD, &x);
+  lis_solver_create (&solver);
+    
+  lis_matrix_set_size (A, n, 0);  
+  lis_matrix_set_csr (nnz, row, col, value, A);
+  lis_vector_set_size (b, n, 0);
+  lis_vector_duplicate (b, &x);
+  LIS_INT ie;
+  lis_vector_get_range (x, &row_s, &ie);
+  assert (ie - rows_s == n);
+  
+  initialized = true;
+  return 1;
+}
+
+int
+lis::assemble_lis_matrix ()
+{
+  for (int i = 0; i < nnz ; ++i)
+    value[i] = data[i];
+  
+  lis_matrix_assemble (A);
+  return 1;
+}
+
+int
+lis::invoke_lis_solver ()
+{
+
+  for (int i = rows_s; i < rows_s + n; ++i)
+    {
+      lis_vector_set_value (LIS_INS_VALUE, i, rhs[i - row_s], b); 
+      if (have_initial_guess)
+        lis_vector_set_value (LIS_INS_VALUE, i, initial_guess[i - row_s], x);
+    }
+
+  if (! option_string_set)
+    {
+      std::stringstream opt;
+      opt << "-maxiter " << max_iter
+          << " -tol " << tolerance
+          << " -i " << iterative_method
+          << " -p " << preconditioner
+          << " -conv_cond " << convergence_condition;
+
+      if (have_initial_guess)
+        opt << " -initx_zeros false ";
+      else
+        opt << " -initx_zeros true ";
+
+      option_string = opt.str ();
+      option_string_set = true;
+    }
+
+  options = std::unique_ptr<char> (new char[option_string.length () + 1]);
+  std::copy (option_string.begin (),
+             option_string.end (), options.get ());
+
+  lis_solver_set_option (options.get (), solver);
+  lis_solve (A, b, x, solver);
+
+  lis_solver_get_iter (solver, &iter);
+  lis_solver_get_time (solver, &time);
+
+  //gather solution vector
+  double temp = 0.0;
+  for (int i = row_s; i < row_s + n; ++i)
+    {
+      lis_vector_get_value (x, i, &temp);
+      rhs[i - row_s] = temp;
+    }
+  
+  return 1;
+}
+
+void
+lis::destroy_lis_objects ()
+{  
+  lis_solver_destroy (solver);
+  lis_matrix_destroy (A);
+  lis_vector_destroy (b);
+  lis_vector_destroy (x);
+}
+
+void
+lis::set_iterative_method (const std::string &s)
+{
+  if (s == "Conjugate Gradient")
+    iterative_method = "cg";
+  else if (s == "Biconjugate Gradient")
+    iterative_method = "bicg";
+  else if (s == "Bicg Stabilized")
+    iterative_method = "bicgstab";
+  else if (s == "Jacobi")
+    iterative_method = "jacobi";
+  else if (s == "Gauss Seidel")
+    iterative_method = "gs";
+  else if (s == "SOR")
+    iterative_method = "sor";
+  else
+    {
+      if (rank == 0)
+        std::cout << std::endl
+                  <<"Invalid Iterative Method"
+                  << std::endl
+                  << "Solve with default BiConjugate Gradient"
+                  << std::endl;
+      iterative_method = "bicg";
+    }
+  option_string_set = false;
+}
+
+
+void
+lis::set_preconditioner (const std::string &s)
+{
+  if (s == "none")
+    preconditioner = "none";
+  else if (s == "jacobi")
+    preconditioner = "jacobi";
+  else if (s == "ssor")
+    preconditioner = "ssor";
+  else if (s == "ilu")
+    preconditioner = "ilu";
+  else if (s == "ilut")
+    preconditioner = "ilut";
+  else if (s == "iluc")
+    preconditioner = "iluc";
+  else
+    {
+      if (rank == 0)
+        std::cout << std::endl
+                  <<"Invalid Preconditioner"
+                  << std::endl
+                  << "Solve with ilu[0]"
+                  << std::endl;
+      preconditioner = "ilu";
+    }
+  option_string_set = false;
+}
+
+void
+lis::set_convergence_condition (const std::string &s)
+{
+  if (s == "norm2_of_residual")
+    convergence_condition = "nrm2_r";
+  else if (s == "norm2_of_rhs")
+    convergence_condition = "nrm2_b";
+  else
+    {
+      if (rank == 0)
+        std::cout << std::endl
+                  <<"Invalid Convergence Condition"
+                  << std::endl
+                  << "Solve with default norm2_of_residual"
+                  << std::endl;
+      convergence_condition = "nrm2_r";
+    }
+  option_string_set = false;
+}
+
+void
+lis::set_lhs_structure
+(int n,
+ std::vector<int> &ir,
+ std::vector<int> &jc)
+{
+  n_row = n;
+  row_ptr.assign (n_row + 1, 0);
+  jcol.assign (jc.size (), 0);
+
+  row_ptr = ir;
+  jcol = jc;
+  nnz = jcol.size ();
+}
+
+int
+lis::analyze () { return 1; }
+
+void
+lis::set_lhs_data (std::vector<double> &xa)
+{ data = &*xa.begin (); }
+
+
+int
+lis::factorize ()
+{   
+  init_lis_objects ();
+  int CHK = assemble_lis_matrix (); assert (CHK == 1);  
+  return CHK;
+}
+
+int
+lis::solve ()
+{
+
+  int retval = invoke_lis_solver ();
+
+  if (verbose)
+    std::cout << std::endl
+              << "Number of iterations = " << iter
+              << std::endl
+              << "Elapsed time = " << time << std::endl;
+
+  return retval;
+}
+
---
+++ src/lis_class_distributed.h
@@ -0,0 +1,270 @@
+/*
+  Copyright (C) 2011 Carlo de Falco
+  This software is distributed under the terms
+  the terms of the GNU/GPL licence v3
+*/
+/*! \file lis_class.h
+  \brief Interface for linear solver built with lis library.
+*/
+
+#ifndef HAVE_LIS_CLASS
+#define HAVE_LIS_CLASS 1
+
+#include <lis.h>
+#include <string>
+#include <bim_sparse.h>
+#include <linear_solver.h>
+
+
+/// Interface for linear solver built with lis library.
+class lis_distributed: public linear_solver
+{
+private :
+
+  LIS_SOLVER solver;
+  LIS_MATRIX A;
+  LIS_VECTOR b, x;
+  LIS_INT iter;
+  double time;
+  bool initialized;
+  
+  LIS_INT *row,  *col;
+  LIS_SCALAR *value;
+
+  /// Stores rows of matrix in CSR format.
+  std::vector<int> row_ptr;
+
+  /// Stores columns of matrix in CSR format
+  std::vector<int> jcol;
+
+  /// Pointer to data of matrix.
+  double *data;
+
+  /// Pointer to values of rhs.
+  double *rhs;
+
+  /// Stores values of ordering map.
+  std::vector<int> ordering_map;
+
+  /// Pointer to values of initial guess.
+  double *initial_guess;
+
+  /// It's true if solver have initial guess passed by user.
+  bool have_initial_guess;
+
+  /// \brief Maximum number of iterations.
+  /// \details [default = 1000].
+  int max_iter;
+
+  /// \brief Tolerance of linear solver.
+  /// \details [default = 1e-12].
+  double tolerance;
+
+  /// \brief Name of iterative method.
+  /// \details [default = bicg].
+  std::string iterative_method;
+
+  /// \brief Name of preconditioner.
+  /// \details [default = none].
+  std::string preconditioner;
+
+  /// \brief Type of convergence condition.
+  /// \details [default = norm 2 of residual]
+  std::string convergence_condition;
+
+  std::vector<int> map_i_s, map_row_s;
+  std::vector<int> map_n, map_nnz;
+
+  int i_s, row_s;
+  int n, nnz, n_row;
+
+  /// Index base used by specific linear solver.
+  static const int index_base = 0;
+
+  int rank, size;
+
+  /// Private helper functions
+  void
+  set_lhs_structure_csr
+  (int n, std::vector<int> &ir, std::vector<int> &jc);
+
+  int
+  init_lis_objects ();
+
+  int
+  assemble_lis_matrix ();
+  
+  int
+  invoke_lis_solver ();
+
+  void
+  destroy_lis_objects ();
+  
+public :
+
+ /*
+ 
+  Typical usage :
+
+  linear_solver *solver = new lis ();
+  solver->set_lhs_structure (lhs.num_owned_rows (), ir, jc);
+  solver->analyze ();
+  solver->set_lhs_data (xa);
+  solver->factorize ();
+  solver->set_rhs (rhs);
+  if (solver->solver_type () == "iterative")
+  solver->set_max_iterations (1000);
+  solver->set_tolerance (1e-12);
+  solver->solve ();
+  solver->cleanup ();
+
+
+*/
+  
+  /// \brief Option string.
+  std::string option_string;
+  bool option_string_set;
+  bool verbose;
+  
+  /// Default costructor.
+  lis (LIS_INT argc = 0, char * argv[] = NULL) :
+    linear_solver ("LIS", "iterative"),
+    initialized (false),
+    row (0),
+    col (0),
+    value (0),
+    data (0),
+    rhs (0),
+    initial_guess (0),
+    have_initial_guess (false),
+    max_iter (1000),
+    tolerance (1.0e-12),
+    iterative_method ("bicg"),
+    preconditioner ("none"),
+    convergence_condition ("nrm2_r"),
+    option_string (""),
+    option_string_set (false),
+    verbose (true)
+  {
+    lis_initialize (&argc, &argv);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+  };
+
+  /// Set-up the matrix structure.
+  void
+  set_lhs_structure (int n, std::vector<int> &ir,
+                     std::vector<int> &jc,
+                     matrix_format_t f = csr);
+
+  /// Perform the analysis.
+  int
+  analyze ();
+  
+  /// Set matrix entries.
+  void
+  set_lhs_data (std::vector<double> &xa);
+
+  /// Set the rhs.
+  void
+  set_rhs (std::vector<double> &rhs_)
+  { rhs = &*rhs_.begin (); }
+
+  /// Set the initial guess.
+  void
+  set_initial_guess (std::vector<double> &initial_guess_)
+  {
+    have_initial_guess = true;
+    initial_guess = &*initial_guess_.begin ();
+  }
+
+  /// Prepare the solver.
+  int
+  factorize ();
+ 
+
+  /// Solve the system.
+  int
+  solve ();
+ 
+
+  /// Cleanup memory.
+  void
+  cleanup () {
+    
+    destroy_lis_objects ();
+    initialized = false;
+    delete[] row;
+    delete[] col;
+    delete[] value;
+
+  };
+
+
+  /// Set maximum number of iterations (default = 1000).
+  void
+  set_max_iterations (int max_iter_)
+  {
+    max_iter = max_iter_;
+    option_string_set = false;
+  }
+
+  /// Get maximum number of iterations.
+  void
+  get_max_iterations (int &max_iter_)
+  {
+    max_iter_ = max_iter;
+    option_string_set = false;
+  }
+
+  /// Set tolerance of iterative method (default = 1.0e-12).
+  void
+  set_tolerance (double tol)
+  {
+    tolerance = tol;
+    option_string_set = false;
+  }
+
+  /// Get tolerance of linear solver.
+  void
+  get_tolerance (double &tol)
+  { tol = tolerance; }
+
+  /// Get type of iterative method.
+  void
+  get_iterative_method (std::string &s)
+  { s = iterative_method; }
+
+  /// Get type of preconditioner.
+  void
+  get_preconditioner (std::string &s)
+  { s = preconditioner; }
+
+  /// get convergence condition of iterative method
+  void
+  get_convergence_condition (std::string &s)
+  { s = convergence_condition; }
+
+
+
+  /// Set type of iterative method (default = cg).
+  void
+  set_iterative_method (const std::string &s);
+
+  /// Set type of preconditioner (default = none).
+  void
+  set_preconditioner (const std::string &s);
+
+  /// Set convergence condition of iterative method
+  /// (default = nrm2_r)
+  void
+  set_convergence_condition (const std::string &s);
+
+  /// LIS uses 0-based indexing
+  inline int
+  get_index_base ()
+  { return index_base; }
+
+  
+};
+#endif
