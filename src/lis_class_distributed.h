96c8b66f69e72d9cab64440ade75ebd2bd6cdec2
Carlo de Falco <carlo.defalco@polimi.it>
2022-11-07 13:48:30 +0100

    lis distributed class

--- src/lis_class_distributed.h
+++ src/lis_class_distributed.h
@@ -10,9 +10,10 @@
 #ifndef HAVE_LIS_CLASS
 #define HAVE_LIS_CLASS 1
 
+#include <iostream>
 #include <lis.h>
 #include <string>
-#include <bim_sparse.h>
+#include <vector>
 #include <linear_solver.h>
 
 
