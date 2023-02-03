e8505a15abe43dd09c8503f463511f67d8521485
federico gatti <federicog@federicos-MBP.Home>
2023-02-03 19:58:46 +0100

    fixed sqrt(porosity)

--- examples/shallow_water/main_TG2IMEX-RKC_Strang_balanced_two_phase.cpp
+++ examples/shallow_water/main_TG2IMEX-RKC_Strang_balanced_two_phase.cpp
@@ -12,7 +12,7 @@
  
 #include <bim_distributed_vector.h>
 #include <bim_timing.h>
-#include <mumps_class.h> 
+#include <mumps_class.h>
 #include <tmesh.h>
 #include <quad_operators.h>
 
@@ -29,9 +29,9 @@ static constexpr char VARNAME_2[255] = "mask_in";
 //static constexpr char VARNAME_3[255] = "mask_fin"; 
 
 // properties of the input dem
-static constexpr double res = .1;//0.005*500; // it is also the minimum resolution of the bim element
-static constexpr double Nx = 101;//449;//101;//165;//201;//188; // # columns
-static constexpr double Ny = 101;//544;//101;//175;//201;//180; // # rows
+static constexpr double res = 10;//0.005*500; // it is also the minimum resolution of the bim element
+static constexpr double Nx = 449;//449;//101;//165;//201;//188; // # columns
+static constexpr double Ny = 544;//544;//101;//175;//201;//180; // # rows
  
   
 static constexpr double L = res*(Nx-1);
@@ -45,8 +45,8 @@ static constexpr int NUM_TREFINEMENTS = 1; // 10
 
 
 
-static constexpr double SPACE_ADAPTDT = .025;//1e-2; // put zero if you want at each time step
-static constexpr double SAVEDT = .1; // must never be null 
+static constexpr double SPACE_ADAPTDT = 1.;//1e-2; // put zero if you want at each time step
+static constexpr double SAVEDT = 5.; // must never be null 
 static constexpr double DELTAT = 1.;
 static constexpr double REDCDT = .8; // it is the limit of the CFL condition
 static constexpr double T      = 230.;
@@ -59,7 +59,7 @@ static constexpr bool is_bed_friction           = true;
 static constexpr bool is_max_time_step_from_CFL = true;
  
 
-static constexpr double h_min = 1.e-3;
+static constexpr double h_min = 1.e-2;
 static constexpr double grav = 9.81;
 
 // variables that can be used for UQ
@@ -67,8 +67,8 @@ static constexpr double density = 2350.;
 static constexpr double density_s = 2700.;
 static constexpr double density_w = 1000.;
 static constexpr double turbulence_coeff = 1e10;
-static constexpr double bed_friction_angle_rad = 0*17.*M_PI/180; //33.9*M_PI/180; //0.0; //23*M_PI/180; 
-static constexpr double erosion_coefficient = 0*5e-5; // 0.
+static constexpr double bed_friction_angle_rad = 17.*M_PI/180; //33.9*M_PI/180; //0.0; //23*M_PI/180; 
+static constexpr double erosion_coefficient = 5e-5; // 0.
 static constexpr double m_coeff = 1.;
 static constexpr double terminal_velocity = 1.e-2; // non può essere nulla!
 
@@ -77,7 +77,7 @@ static constexpr double level_wet           = 3;
 static constexpr double level_interface     = 6; // minimum resolution!  
 static constexpr double mesh_size_dry       = res*1e3;//res/60*std::pow(2,level_interface); //res*std::pow(2,level_interface); 
 static constexpr double mesh_size_wet       = res;///10;//res/20;//res;//mesh_size_dry/std::pow(2,level_wet); // finest resolution
-static constexpr double mesh_size_interface = res;//res/30;//res/60;//mesh_size_dry/std::pow(2,level_interface);
+static constexpr double mesh_size_interface = res/2;//res/30;//res/60;//mesh_size_dry/std::pow(2,level_interface);
  
 
 // Connectivity of local element
