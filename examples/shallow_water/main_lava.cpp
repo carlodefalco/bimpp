7bc73eeee52089d29aa8d45315c5f86ca22396a8
Federico Gatti <federico@MacBook-Air-di-IMATI-5.local>
2024-01-18 17:05:54 +0100

    a

--- examples/shallow_water/main_lava.cpp
+++ examples/shallow_water/main_lava.cpp
@@ -127,12 +127,14 @@ double dem_fun (const double& xx, const double& yy)
   //return ( 1.+.1*std::exp(-0.5*( std::pow(xx-L/2.,2.) )/std::pow(0.2*L/2.,2.) ) );
   //return(-xx+L);
   double z = 0.;
-  const double r = std::sqrt((xx-100)*(xx-100) + (yy-100)*(yy-100));
-  if (r<=10)
+  const double rx = std::abs(xx-L/2.);
+  const double ry = std::abs(yy-H/2.); 
+  const double r = std::sqrt((xx-L/2.)*(xx-L/2.) + (yy-H/2.)*(yy-H/2.));
+  if (rx<=10 && ry<= 10)
   {
     z = 60;
   }
-  else if (r>10 && r<=50)
+  else if (rx>10 && rx<=50 && ry>10 && ry<=50)
   {
     z = -r + 70.;
   }
@@ -167,10 +169,10 @@ using Q0  = distributed_vector; //distributed_vector; //std::vector<double>;    
 //double h0_fun (const double& xx, const double& yy)  { return std::max (0., (8. - std::sin (M_PI * xx / 2. / 400.) - dem[global_coord_2_raster(xx,yy)[0]])); }
 double h0_fun (const double& xx, const double& yy) 
 {
-  //return(0.);
+  return(0.);
   //return(xx/L*1500);
   //return( std::abs(xx-L/2.)<=7. && std::abs(yy-H/2.)<=7.5 ? 3 : 0. );
-  return(std::sqrt(std::pow(xx-L/2.,2.) + std::pow(yy-H/2.,2.))<=7.5 ? 3. : 0.  );
+  //return(std::sqrt(std::pow(xx-L/2.,2.) + std::pow(yy-H/2.,2.))<=7.5 ? 3. : 0.  );
   //return (xx<=L/2. && xx>=L/4. ? 3. : 0.);
   //return ( 1.+.1*std::exp(-0.5*( std::pow(xx-L/2.,2.) )/std::pow(0.2*L/2.,2.) ) );
   //return ( 1.+1.*std::exp(-0.5*( std::pow(yy-H/2.,2.) )/std::pow(0.2*L/2.,2.) ) );
