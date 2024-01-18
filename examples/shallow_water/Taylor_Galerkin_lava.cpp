edb353b679c4ee45f439ca0c2cde6d68c6ec6e0b
Federico Gatti <federico@MacBook-Air-di-IMATI-5.local>
2024-01-18 16:33:41 +0100

    a

--- examples/shallow_water/Taylor_Galerkin_lava.cpp
+++ examples/shallow_water/Taylor_Galerkin_lava.cpp
@@ -1022,11 +1022,11 @@ TG2_scheme::solve_non_lin(const int& kk)
   h_c += dt*incr.get_owned_data ()[kk]/mass.get_owned_data ()[kk];
   h_c *= (h_c>0.);
 
-  Ux_c += dt*incr.get_owned_data ()[kk+1]/mass.get_owned_data ()[kk+1];// + dt*.5*Ux_src_formula(h_c_old, Ux_c_old, 0., Th_c_old);
-  Uy_c += dt*incr.get_owned_data ()[kk+2]/mass.get_owned_data ()[kk+2];// + dt*.5*Uy_src_formula(h_c_old, 0., Uy_c_old, Th_c_old);
+  Ux_c += dt*incr.get_owned_data ()[kk+1]/mass.get_owned_data ()[kk+1] + dt*.5*Ux_src_formula(h_c_old, Ux_c_old, 0., Th_c_old);
+  Uy_c += dt*incr.get_owned_data ()[kk+2]/mass.get_owned_data ()[kk+2] + dt*.5*Uy_src_formula(h_c_old, 0., Uy_c_old, Th_c_old);
   
-  //Ux_c = h_c>epsilon ? Ux_c/(1.-dt*.5*Ux_src_formula(h_c, 1., 0., Th_c_old)) : 0.;
-  //Uy_c = h_c>epsilon ? Uy_c/(1.-dt*.5*Uy_src_formula(h_c, 0., 1., Th_c_old)) : 0.;
+  Ux_c = h_c>epsilon ? Ux_c/(1.-dt*.5*Ux_src_formula(h_c, 1., 0., Th_c_old)) : 0.;
+  Uy_c = h_c>epsilon ? Uy_c/(1.-dt*.5*Uy_src_formula(h_c, 0., 1., Th_c_old)) : 0.;
 
   //Ux_c = (Ux_c + dt*incr.get_owned_data ()[kk+1]/mass.get_owned_data ()[kk+1] + dt*.5*Ux_src_formula(h_c_old, Ux_c_old, 0., Th_c_old) )/(1.-dt*.5*Ux_src_formula(h_c, 1., 0., Th_c_old));
   //Uy_c = (Uy_c + dt*incr.get_owned_data ()[kk+2]/mass.get_owned_data ()[kk+2] + dt*.5*Uy_src_formula(h_c_old, 0., Uy_c_old, Th_c_old) )/(1.-dt*.5*Uy_src_formula(h_c, 0., 1., Th_c_old));
