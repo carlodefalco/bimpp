e48125e0c5e32d283c81b281a14d5725584564b5
Federico Gatti <federico@Air-di-IMATI-5.Home>
2024-01-26 11:55:54 +0100

    a

--- examples/shallow_water/Taylor_Galerkin_lava.cpp
+++ examples/shallow_water/Taylor_Galerkin_lava.cpp
@@ -233,13 +233,6 @@ TG2_scheme::first_step (tmesh::quadrant_iterator quadrant)
   sol_onehalf[ordUy   (index_quadrant_global)] = Uy_cell_average - dt*.5 * (div_FUy_cell/area - src_slope_formula (h_cell_average, slope_y_c));
   sol_onehalf[ordTh   (index_quadrant_global)] = Th_cell_average - dt*.5 *  div_FTh_cell/area;
   
-  // add source term for the momentum
-  const auto & h_onehalf_updated = sol_onehalf[ordh    (index_quadrant_global)];
-
-  sol_onehalf[ordUx   (index_quadrant_global)] = sol_onehalf[ordUx   (index_quadrant_global)]/(1. - dt*.5*Ux_src_formula(h_onehalf_updated, 1., 0., Th_cell_average));
-  sol_onehalf[ordUy   (index_quadrant_global)] = sol_onehalf[ordUy   (index_quadrant_global)]/(1. - dt*.5*Uy_src_formula(h_onehalf_updated, 0., 1., Th_cell_average));
-  
-
   // add the vent contribution,
   const double delta_x_vc = x_c - x_v;
   const double delta_y_vc = y_c - y_v;
@@ -252,6 +245,12 @@ TG2_scheme::first_step (tmesh::quadrant_iterator quadrant)
 
   sol_onehalf[ordh    (index_quadrant_global)] += dt*.5* Q_vent/area*       ( std::erf(extr_x_b) - std::erf(extr_x_a) )/2.*( std::erf(extr_y_b) - std::erf(extr_y_a) )/2.;
   sol_onehalf[ordTh   (index_quadrant_global)] += dt*.5* Q_vent/area*T_vent*( std::erf(extr_x_b) - std::erf(extr_x_a) )/2.*( std::erf(extr_y_b) - std::erf(extr_y_a) )/2.;
+
+  // add source term for the momentum
+  const auto & h_onehalf_updated = sol_onehalf[ordh    (index_quadrant_global)];
+
+  sol_onehalf[ordUx   (index_quadrant_global)] = sol_onehalf[ordUx   (index_quadrant_global)]/(1. - dt*.5*Ux_src_formula(h_onehalf_updated, 1., 0., Th_cell_average));
+  sol_onehalf[ordUy   (index_quadrant_global)] = sol_onehalf[ordUy   (index_quadrant_global)]/(1. - dt*.5*Uy_src_formula(h_onehalf_updated, 0., 1., Th_cell_average));
 } 
 
 
